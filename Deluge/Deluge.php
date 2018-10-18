ae898730451ef095982dcea300f0ca43615b6f3f
Chris <chris@headred.net>
2018-10-18 10:53:40 +0100

    change files

--- Deluge/Deluge.php
+++ Deluge/Deluge.php
@@ -3,52 +3,25 @@
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Client;
 
-class Deluge {
+class Deluge extends SupportedApps implements EnhancedApps {
 
-    public function test()
-    {
-        $res = $this->login()[0];
-        switch($res->getStatusCode()) {
-            case 200:
-                $data = json_decode($res->getBody());
-                if(!isset($data->result) || is_null($data->result) || $data->result == false) {
-                    echo 'Failed: Invalid Credentials';
-                } else {
-                    echo 'Successfully connected to the API';
-                }
-                break;
-            case 401:
-                echo 'Failed: Invalid credentials';
-                break;
-            case 404:
-                echo 'Failed: Please make sure your URL is correct and that there is a trailing slash';
-                break;
-            default:
-                echo 'Something went wrong... Code: '.$res->getStatusCode();
-                break;
-        }
-    }
-    public function execute()
+    //public function test()
+    //public function execute()
+
+
+    public function livestats()
     {
         $html = '';
         $active = 'active';
         $jar = $this->login()[1];
         $res = $this->getDetails($jar);
-        $data = json_decode($res->getBody());
-        $download_rate = $data->result->stats->download_rate;
-        $upload_rate = $data->result->stats->upload_rate;
-        $seed_count = $data->result->filters->state[2];
-        $leech_count = $data->result->filters->state[1];
-        $html = '
-        <ul class="livestats">
-            <li><span class="title"><i class="fas fa-arrow-down"></i> '.$this->formatBytes($download_rate).'</span></li>
-            <li><span class="title"><i class="fas fa-arrow-up"></i> '.$this->formatBytes($upload_rate).'</span></li>
-        </ul>
-        <ul class="livestats">
-            <li><span class="title">Leech: '.$leech_count[1].'</span></li>
-            <li><span class="title">Seed: '.$seed_count[1].'</span></li>
-        </ul>
-        ';
+        $response = json_decode($res->getBody());
+        $data['download_rate'] = $this->formatBytes($response->result->stats->download_rate);
+        $data['upload_rate'] = $this->formatBytes($response->result->stats->upload_rate);
+        $data['seed_count'] = $response->result->filters->state[2][1];
+        $data['leech_count'] = $response->result->filters->state[1][1];
+        $html = view('SupportedApps::Deluge.livestats')->with('products', $products)->render();
+        
         return json_encode(['status' => $active, 'html' => $html]);
     }
     public function getDetails($jar)
@@ -64,22 +37,6 @@ class Deluge {
             'headers'  => ['content-type' => 'application/json', 'Accept' => 'application/json']
         ]);
         return $res;
-    }
-    public function login()
-    {
-        $config = $this->config;
-        $url = $config->url;
-        $password = $config->password;
-        $url = rtrim($url, '/');
-        $api_url = $url.'/json';
-        $jar = new \GuzzleHttp\Cookie\CookieJar();
-        $client = new Client(['http_errors' => false, 'timeout' => 15, 'connect_timeout' => 15]);
-        $res = $client->request('POST', $api_url, [
-            'body' => '{"method": "auth.login", "params": ["'.$password.'"], "id": 1}',
-            'cookies' => $jar,
-            'headers'  => ['content-type' => 'application/json', 'Accept' => 'application/json']
-        ]);
-        return array($res,$jar);
     }
 
     function formatBytes($bytes, $precision = 2) { 
---
+++ Deluge/livestats.blade.php
@@ -0,0 +1,8 @@
+<ul class="livestats">
+    <li><span class="title"><i class="fas fa-arrow-down"></i>{{ $download_rate }}</span></li>
+    <li><span class="title"><i class="fas fa-arrow-up"></i>{{ $upload_rate }}</span></li>
+</ul>
+<ul class="livestats">
+    <li><span class="title">Leech: {{ $leech_count }}</span></li>
+    <li><span class="title">Seed: {{ $seed_count }}</span></li>
+</ul>
