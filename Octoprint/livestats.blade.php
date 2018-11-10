43b7a12a3cde6ad71f7ad0788d603e5262abc7df
Kode <kodestar@gmail.com>
2018-11-10 21:15:34 +0000

    Add Octoprint enhanced app

---
+++ Octoprint/livestats.blade.php
@@ -0,0 +1,10 @@
+<ul class="livestats">
+    <li>
+        <span class="title">Progress</span>
+        <strong>{!! $progress !!}<span>%</span></strong>
+    </li>
+    <li>
+        <span class="title">Finish</span>
+        <strong>{!! $estimated !!}</strong>
+    </li>
+</ul>
