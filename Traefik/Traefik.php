8bd7ad0d7494a3c9bff1a81a5017ee64d1ed767a
cenci0 <6401170+cenci0@users.noreply.github.com>
2019-10-01 22:17:35 +0200

    Upgrade Traefik enhanced app to support Traefik v2
    Add configuration for the Traefik v2 API.
    This app in no longer compatible with Traefik v1 /health endpoint.

--- Traefik/Traefik.php
+++ Traefik/Traefik.php
@@ -1,40 +1,162 @@
 <?php namespace App\SupportedApps\Traefik;
 
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
+use Illuminate\Support\Collection;
+
 class Traefik extends \App\SupportedApps implements \App\EnhancedApps {
 
-    public $config;
+    public function getRequestAttrs()
+    {
+        $username = $this->getConfigValue('username', null);
+        $password = $this->getConfigValue('password', null);
+        $ignoreTls = $this->getConfigValue('ignore_tls', false);
 
-    //protected $login_first = true; // Uncomment if api requests need to be authed first
-    //protected $method = 'POST';  // Uncomment if requests to the API should be set by POST
+        $isLoginNeeded =
+            !is_null($username) && !empty($username)
+            && !is_null($password) && !empty($password);
 
-    function __construct() {
-        //$this->jar = new \GuzzleHttp\Cookie\CookieJar; // Uncomment if cookies need to be set
+        $attrs['headers'] = [ 'Accept' => 'application/json'];
+        if($isLoginNeeded) {
+            $attrs['auth'] = [ $username, $password ];
+        }
+        if($ignoreTls) {
+            $attrs['verify'] = false;
+        }
+
+        return $attrs;
     }
 
     public function test()
     {
-        $test = parent::appTest($this->url('health'));
+        $attrs = $this->getRequestAttrs();
+        $test = parent::appTest($this->url('api/version'), $attrs);
+
         echo $test->status;
     }
 
     public function livestats()
     {
-        $status = 'inactive';
-        $res = parent::execute($this->url('health'));
-        $details = json_decode($res->getBody());
+        $apiEndpoints = collect([
+            'httpRouters' => 'api/http/routers',
+            'httpServices' => 'api/http/services',
+            'tcpRouters' => 'api/tcp/routers',
+            'tcpServices' => 'api/tcp/services'
+        ]);
 
-        $data = [];
+        $status = 'active';
+        $attrs = $this->getRequestAttrs();
 
-        $avg_response_time = $details->average_response_time_sec ?? 0;
-        $time = $avg_response_time*1000;
-        $data['time_output'] = ($time > 0) ? number_format($time, 2).'<span>ms</span>' : 'Unknown';
 
+        $responses = $apiEndpoints->mapWithKeys(function ($endpoint, $key) use ($attrs) {
+            $response = parent::execute($this->url($endpoint), $attrs);
+            $body = json_decode($response->getBody());
+            $bodyCollection = collect($body);
+
+            return [ $key => [
+                'data' => $bodyCollection->filter(function ($value, $key) { return $value->status === 'enabled'; })->count(),
+                'total' => $bodyCollection->count(),
+            ] ];
+        });
+
+
+        $data = $this->getViewData($this->getConfigValue('fields', 'E'), $responses);
         return parent::getLiveStats($status, $data);
-        
     }
+
+    public function getViewData($config, $responses)
+    {
+        $nullValue = [ 'data' => 0, 'total' => 0 ];
+
+        switch ($config) {
+            /* HTTP routers/services only */
+            case 'H':
+                return [
+                    'left' => [
+                        'title' => 'Routers',
+                        'data' => $responses->get('httpRouters', $nullValue)['data'],
+                        'total' => $responses->get('httpRouters', $nullValue)['total'],
+                    ],
+                    'right' => [
+                        'title' => 'Services',
+                        'data' => $responses->get('httpServices', $nullValue)['data'],
+                        'total' => $responses->get('httpServices', $nullValue)['total'],
+                    ],
+                ];
+                break;
+
+            /* TCP routers/services only */
+            case 'T':
+                return [
+                    'left' => [
+                        'title' => 'Routers',
+                        'data' => $responses->get('tcpRouters', $nullValue)['data'],
+                        'total' => $responses->get('tcpRouters', $nullValue)['total'],
+                    ],
+                    'right' => [
+                        'title' => 'Services',
+                        'data' => $responses->get('tcpServices', $nullValue)['data'],
+                        'total' => $responses->get('tcpServices', $nullValue)['total'],
+                    ],
+                ];
+
+            /* Routers only */
+            case 'R':
+                return [
+                    'left' => [
+                        'title' => 'HTTP',
+                        'data' => $responses->get('httpRouters', $nullValue)['data'],
+                        'total' => $responses->get('httpRouters', $nullValue)['total'],
+                    ],
+                    'right' => [
+                        'title' => 'TCP',
+                        'data' => $responses->get('tcpRouters', $nullValue)['data'],
+                        'total' => $responses->get('tcpRouters', $nullValue)['total'],
+                    ],
+                ];
+
+            /* Services only */
+            case 'S':
+                return [
+                    'left' => [
+                        'title' => 'HTTP',
+                        'data' => $responses->get('httpServices', $nullValue)['data'],
+                        'total' => $responses->get('httpServices', $nullValue)['total'],
+                    ],
+                    'right' => [
+                        'title' => 'TCP',
+                        'data' => $responses->get('tcpServices', $nullValue)['data'],
+                        'total' => $responses->get('tcpServices', $nullValue)['total'],
+                    ],
+                ];
+            
+            /* Everything */
+            default:
+                return [
+                    'left' => [
+                        'title' => 'Routers',
+                        'data' => $responses->get('httpRouters', $nullValue)['data'] + $responses->get('tcpRouters', $nullValue)['data'],
+                        'total' => $responses->get('httpRouters', $nullValue)['total'] + $responses->get('tcpRouters', $nullValue)['total'],
+                    ],
+                    'right' => [
+                        'title' => 'Services',
+                        'data' => $responses->get('httpServices', $nullValue)['data'] + $responses->get('tcpServices', $nullValue)['data'],
+                        'total' => $responses->get('httpServices', $nullValue)['total'] + $responses->get('tcpServices', $nullValue)['total'],
+                    ],
+                ];
+        }
+    }
+
     public function url($endpoint)
     {
         $api_url = parent::normaliseurl($this->config->url).$endpoint;
         return $api_url;
     }
+
+    public function getConfigValue($key, $default=null)
+    {
+        return (isset($this->config) && isset($this->config->$key)) ? $this->config->$key : $default;
+    }
+
 }
+
