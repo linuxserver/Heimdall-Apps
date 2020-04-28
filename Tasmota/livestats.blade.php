61e4c0448d339788e49abd87545e9973500e1298
Tom Collis <tomuky2k@users.noreply.github.com>
2020-04-28 10:27:59 +0100

    Initial Config Change for Live Stats

--- Tasmota/livestats.blade.php
+++ Tasmota/livestats.blade.php
@@ -1,10 +1,10 @@
 <ul class="livestats">
     <li>
-        <span class="title">Queue</span>
-        <strong>{!! $queue_size !!}</strong>
+        <span class="title">Temperature</span>
+        <strong>{!! $temperature !!}</strong>
     </li>
     <li>
-        <span class="title">Speed</span>
-        <strong>{!! $current_speed !!}</strong>
+        <span class="title">Humidity</span>
+        <strong>{!! $humidity !!}</strong>
     </li>
 </ul>
