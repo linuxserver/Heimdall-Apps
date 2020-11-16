91a8296745ab0d95bae5ed2b109ceda3696e5fd9
Niphram <theniphram@gmail.com>
2020-11-16 23:34:44 +0100

    Fix bug when adding the application

--- Nextcloud/config.blade.php
+++ Nextcloud/config.blade.php
@@ -2,11 +2,11 @@
 <div class="items">
     <div class="input">
         <label>{{ strtoupper(__('app.url')) }}</label>
-        {!! Form::text('config[override_url]', null, array('placeholder' => __('app.apps.override'), 'id' => 'override_url', 'class' => 'form-control')) !!}
+        {!! Form::text('config[override_url]', isset($item) ? ($item->getConfig()->override_url ?? null) : null, array('placeholder' => __('app.apps.override'), 'id' => 'override_url', 'class' => 'form-control')) !!}
     </div>
     <div class="input">
         <label>{{ __('app.apps.username') }}</label>
-        {!! Form::text('config[username]', null, array('placeholder' => __('app.apps.username'), 'data-config' => 'username', 'class' => 'form-control config-item')) !!}
+        {!! Form::text('config[username]', isset($item) ? ($item->getConfig()->username ?? null) : null, array('placeholder' => __('app.apps.username'), 'data-config' => 'username', 'class' => 'form-control config-item')) !!}
     </div>
     <div class="input">
         <label>{{ __('app.apps.password') }}</label>
@@ -15,7 +15,7 @@
     </div>
     <div class="input">
         <label>Stats to show</label>
-        {!! Form::select('config[availablestats][]', App\SupportedApps\Nextcloud\Nextcloud::getAvailableStats(), $item->getConfig()->availablestats ?? null, array('multiple'=>'multiple')) !!}
+        {!! Form::select('config[availablestats][]', App\SupportedApps\Nextcloud\Nextcloud::getAvailableStats(), isset($item) ? ($item->getConfig()->availablestats ?? null) : null, array('multiple'=>'multiple')) !!}
     </div>
     <div class="input">
         <button style="margin-top: 32px;" class="btn test" id="test_config">Test</button>
