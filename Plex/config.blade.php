a51643af8959c1ce380820e87bb363cc7f5548b2
Ryan Conrad <camalot@gmail.com>
2020-11-19 00:32:51 -0600

    added help link

--- Plex/config.blade.php
+++ Plex/config.blade.php
@@ -6,7 +6,7 @@
         {!! Form::text('config[override_url]', (isset($item) ? $item->getconfig()->override_url : null), array('placeholder' => __('app.apps.override'), 'id' => 'override_url', 'class' => 'form-control')) !!}
     </div>
 <div class="input">
-        <label title="">Plex Token (help?)</label>
+        <label title="">Plex Token (<a href="https://support.plex.tv/articles/204059436-finding-an-authentication-token-x-plex-token/" target="_blank">help?</a>)</label>
         {!! Form::text('config[token]', (isset($item) ? $item->getconfig()->token : null), array('placeholder' => __('token'), 'data-config' => 'token', 'class' => 'form-control config-item')) !!}
     </div>
     <div class="input">
