aa21c2d6273ad1362ee35bef2b76e908928d0e29
Kode <kodestar@gmail.com>
2021-01-20 13:12:05 +0000

    generate apps list

---
+++ index.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+const glob = require('glob');
+const { hashElement } = require('folder-hash');
+
+glob("**/*.json", {"ignore":['list.json']}, function (err, files) {
+  if(err) {
+    console.log("cannot read the folder, something goes wrong with glob", err)
+  }
+
+  let apptotal = files.length
+  let apps = []
+  let promises = [];
+
+  files.forEach(function(file) {
+    
+    const options = {
+      algho: 'sha1',
+      encoding: 'hex'
+    };
+
+    let folder = file.replace('/app.json', '')
+
+    promises.push(
+      hashElement(folder, options)
+      .then(hash => {
+        console.log(hash.toString())
+        fs.readFile(file, 'utf8', function(err, filedata) {
+          if(err) {
+            console.log("cannot read file", err)
+          }
+          let parsed = JSON.parse(filedata)
+          parsed.sha = hash.hash
+          apps.push(parsed)
+  
+        }) 
+      })
+      .catch(error => {
+        return console.error('hashing failed:', error);
+      })
+    )
+  })
+
+  Promise.all(promises).then(() => {
+    let json = {
+      appcount: apptotal,
+      apps: apps
+    }
+
+    let data = JSON.stringify(json);
+    fs.writeFileSync('list.json', data);
+  });
+
+
+})
