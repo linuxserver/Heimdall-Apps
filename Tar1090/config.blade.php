bf40ad4771adad96391db37cb3fe38383227a175
SingingDwarf <SingingDwarf@users.noreply.github.com>
2021-04-04 23:46:12 +0100

    removed unnecessary config options for Tar1090

--- Tar1090/config.blade.php
+++ Tar1090/config.blade.php
@@ -5,6 +5,7 @@
         <label>{{ strtoupper(__('app.url')) }}</label>
         {!! Form::text('config[override_url]', null, array('placeholder' => __('app.apps.override'), 'id' => 'override_url', 'class' => 'form-control')) !!}
     </div>
+<!--
     <div class="input">
         <label>{{ __('app.apps.username') }}</label>
         {!! Form::text('config[username]', null, array('placeholder' => __('app.apps.username'), 'data-config' => 'username', 'class' => 'form-control config-item')) !!}
@@ -13,6 +14,7 @@
         <label>{{ __('app.apps.password') }}</label>
         {!! Form::text('config[password]', null, array('placeholder' => __('app.apps.password'), 'data-config' => 'password', 'class' => 'form-control config-item')) !!}
     </div>
+-->
     <div class="input">
         <button style="margin-top: 32px;" class="btn test" id="test_config">Test</button>
     </div>
