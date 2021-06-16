0de6171945e84eed5d40eb36a935cb3a3482da7c
Alex <66680498+CMBoii@users.noreply.github.com>
2021-06-16 18:20:27 +0200

    revert back

--- Mealie/Mealie.php
+++ Mealie/Mealie.php
@@ -11,40 +11,25 @@ class Mealie extends \App\SupportedApps implements \App\EnhancedApps {
         //$this->jar = new \GuzzleHttp\Cookie\CookieJar; // Uncomment if cookies need to be set
     }
 
+    public function test()
+    {
+        $test = parent::appTest($this->url('status'));
+        echo $test->status;
+    }
+
     public function livestats()
     {
         $status = 'inactive';
-
-        $res_links = parent::execute($this->url('api/v1/links'), $this->attrs());
-        $links = json_decode($res_links->getBody(), True);
-
-        $res_tags = parent::execute($this->url('api/v1/tags'), $this->attrs());
-        $tags = json_decode($res_tags->getBody(), True);
+        $res = parent::execute($this->url('status'));
+        $details = json_decode($res->getBody());
 
         $data = [];
-
-        if($links) {
-          $data['links'] = $links['total'] ?? 0;
-        }
-
-        if($tags) {
-          $data['tags'] = $tags['total'] ?? 0;
-        }
-
         return parent::getLiveStats($status, $data);
+
     }
     public function url($endpoint)
     {
         $api_url = parent::normaliseurl($this->config->url).$endpoint;
         return $api_url;
-    }
-
-    public function attrs()
-    {
-        $apikey = $this->config->apikey;
-        $attrs = [
-          'headers'  => ['content-type' => 'application/json', 'Authorization' => 'Bearer '.$apikey]
-        ];
-        return $attrs;
     }
 }
