7a0138adf38e8438bc002ff582450503cc1b859e
KodeStar <kodestar@gmail.com>
2021-06-24 15:35:13 +0100

    remove numbers from names

--- n8n/n8n.php
+++
@@ -1,5 +0,0 @@
-<?php namespace App\SupportedApps\n8n;
-
-class n8n extends \App\SupportedApps {
-
-}
---
+++ neightn/neightn.php
@@ -0,0 +1,5 @@
+<?php namespace App\SupportedApps\neightn;
+
+class neightn extends \App\SupportedApps {
+
+}
