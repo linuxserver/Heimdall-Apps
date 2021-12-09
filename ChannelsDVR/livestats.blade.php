152c666c7bab8ab1e8be0d51f1ab079f2d50f595
hcuk94 <me@hc.me.uk>
2021-12-09 19:55:09 +0000

    added enhanced app ChannelsDVR

---
+++ ChannelsDVR/livestats.blade.php
@@ -0,0 +1,10 @@
+<ul class="livestats">
+    <li>
+        <span class="title">Streams</span>
+        <strong>{!! $streams !!}</strong>
+    </li>
+    <li>
+        <span class="title">Recording</span>
+        <strong>{!! $recordings !!}</strong>
+    </li>
+</ul>
