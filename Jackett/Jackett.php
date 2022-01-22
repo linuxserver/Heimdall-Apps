26e061fbfd6849ddedb9ef30fed6521afd457c3a
Anthony Stirling <77850077+Frooodle@users.noreply.github.com>
2022-01-22 18:29:12 +0000

    Add files via upload

--- Jackett/Jackett.php
+++ Jackett/Jackett.php
@@ -1,11 +1,40 @@
 <?php namespace App\SupportedApps\Jackett;
 
-class Jackett extends \App\SupportedApps implements \App\SearchInterface {
-    public $type = 'external'; // Whether to go to the external site or display results locally
-    public function getResults($query, $provider)
+class Jackett extends \App\SupportedApps implements \App\EnhancedApps {
+
+    public $config;
+
+    function __construct() {
+    }
+
+    public function test()
     {
-        $url = rtrim($provider->url, '/');
-        $q = urlencode($query);
-        return redirect($url.'#search='.$q);
+        $test = parent::appTest($this->url('all'));
+        echo $test->status;
+    }
+
+    public function livestats()
+    {
+        $status = 'inactive';
+		
+        $resFailed = parent::execute($this->url('!test:passed'));
+        $detailsFailed = json_decode($resFailed->getBody());  
+		$failed = count($detailsFailed->Indexers) ?? 0;
+		
+		$resAll = parent::execute($this->url('all'));
+        $detailsAll = json_decode($resAll->getBody());  
+		$all = count($detailsAll->Indexers) ?? 0;
+		
+		$working = $all - $failed;
+		$data = []; 
+		$data['indexer_status'] = ''.$working.' / '.$all.'';
+        return parent::getLiveStats($status, $data);
+        
+    }
+    public function url($endpoint)
+    {
+		$apikey = $this->config->apikey;
+        $api_url = parent::normaliseurl($this->config->url).'api/v2.0/indexers/'.$endpoint.'/results?apikey='.$apikey.'&query=Indexers';
+        return $api_url;
     }
 }
---
+++ Jackett/livestats.blade.php
@@ -0,0 +1,6 @@
+<ul class="livestats">
+    <li>
+        <span class="title">Working Indexers</span>
+        <strong>{!! $indexer_status !!}</strong>
+    </li>
+</ul>
