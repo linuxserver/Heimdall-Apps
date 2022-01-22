d9c0228832066f6eaadf9c4f1d35f7f41e9277ed
Anthony Stirling <77850077+Frooodle@users.noreply.github.com>
2022-01-22 13:29:11 +0000

    Update Jenkins.php

--- Jenkins/Jenkins.php
+++ Jenkins/Jenkins.php
@@ -4,7 +4,7 @@ class Jenkins extends \App\SupportedApps implements \App\EnhancedApps {
 
     public $config;
 
-    protected $login_first = true; // Uncomment if api requests need to be authed first
+    //protected $login_first = true; // Uncomment if api requests need to be authed first
     //protected $method = 'POST';  // Uncomment if requests to the API should be set by POST
 
     //  computer/api/xml?tree=computer[executors[currentExecutable[url]]]&xpath=//url&wrapper=builds
@@ -15,7 +15,7 @@ class Jenkins extends \App\SupportedApps implements \App\EnhancedApps {
 
     public function test()
     {
-		if ($this->config->username != '' || $this->config->password != '') {
+	if ($this->config->username != '' || $this->config->password != '') {
             $this->attrs = ['auth'=> [$this->config->username, $this->config->password, 'Basic']];
         }
         $test = parent::appTest($this->url('api/json'),$this->attrs);
@@ -25,32 +25,20 @@ class Jenkins extends \App\SupportedApps implements \App\EnhancedApps {
     public function livestats()
     {
         $status = 'inactive';
-		$data = [];
-		$data['TotalRunningJobs'] = 0;
-		if ($this->config->username != '' || $this->config->password != '') {
+	$data = [];
+	$data['TotalRunningJobs'] = 0;
+	if ($this->config->username != '' || $this->config->password != '') {
             $this->attrs = ['auth'=> [$this->config->username, $this->config->password, 'Basic']];
         }
         $res = parent::execute($this->url('computer/api/xml?tree=computer[executors[currentExecutable[url]]]&depth=1&xpath=//url&wrapper=buildUrls'), $this->attrs);
 		
-		try{
-			$value = simplexml_load_string($res->getBody());
-			$data['TotalRunningJobs'] = count($value->url) ?? 0;
-		} catch(\ErrorException $e) {
-			$data['TotalRunningJobs'] = 0;
-		}
-		
-		
-		/*
-		$details = json_decode($res->getBody());
-        if($details) {
-            $data['TotalRunningJobs'] = count($details->computer[0]->executors) ?? 5;
-            $status = 'active';
-        } else 
-		{
-			$data['TotalRunningJobs'] = 99;
-		}
-		*/
-		
+	try{
+		$value = simplexml_load_string($res->getBody());
+		$data['TotalRunningJobs'] = count($value->url) ?? 0;
+	} catch(\ErrorException $e) {
+		$data['TotalRunningJobs'] = 0;
+	}
+	
         return parent::getLiveStats($status, $data);
         
     }
