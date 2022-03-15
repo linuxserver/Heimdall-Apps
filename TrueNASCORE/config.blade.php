05458f14b63396e1094f5f6888422355f7533d55
Kode <kodestar@gmail.com>
2022-03-15 20:28:42 +0000

    fix a bunch of default settings

--- TrueNASCORE/config.blade.php
+++ TrueNASCORE/config.blade.php
@@ -2,11 +2,11 @@
 <div class="items">
     <div class="input">
         <label>{{ strtoupper(__('app.url')) }}</label>
-        {!! Form::text('config[override_url]', null, array('placeholder' => __('app.apps.override'), 'id' => 'override_url', 'class' => 'form-control')) !!}
+        {!! Form::text('config[override_url]', (isset($item) ? $item->getconfig()->override_url : null), array('placeholder' => __('app.apps.override'), 'id' => 'override_url', 'class' => 'form-control')) !!}
     </div>
     <div class="input">
        <label>{{ __('app.apps.apikey') }}</label>
-        {!! Form::password('config[apikey]', array('placeholder' => __('app.apps.apikey'), 'data-config' => 'apikey', 'class' => 'form-control config-item')) !!}
+        {!! Form::password('config[apikey]', (isset($item) ? $item->getconfig()->password : null), array('placeholder' => __('app.apps.apikey'), 'data-config' => 'apikey', 'class' => 'form-control config-item')) !!}
     </div>
     <div class="input">
         <button style="margin-top: 32px;" class="btn test" id="test_config">Test</button>
