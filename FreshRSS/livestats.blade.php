4218bfcb7733d723fb09951a881938bbe06171a3
Daniel Gibbs <me@danielgibbs.co.uk>
2022-03-26 23:21:53 +0000

    Format all livestats.blade files

--- FreshRSS/livestats.blade.php
+++ FreshRSS/livestats.blade.php
@@ -1,7 +1,7 @@
 <ul class="livestats">
-    @if($unread > 0)
-    <li>
-        <span class="title" style="color:orange;">{!! $unread !!} Unread</span>
-    </li>
+    @if ($unread > 0)
+        <li>
+            <span class="title" style="color:orange;">{!! $unread !!} Unread</span>
+        </li>
     @endif
 </ul>
