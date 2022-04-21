0dc01c3100fc90b13a41326ee6d574d5ed213bbf
Daniel Gibbs <me@danielgibbs.co.uk>
2022-04-21 22:40:03 +0100

    tidy

--- ChannelsDVR/ChannelsDVR.php
+++ ChannelsDVR/ChannelsDVR.php
@@ -1,44 +1,44 @@
 <?php namespace App\SupportedApps\ChannelsDVR;
 
-function preg_grep_keys($pattern, $input) {
-  return preg_grep($pattern, array_keys($input));
+function preg_grep_keys($pattern, $input)
+{
+	return preg_grep($pattern, array_keys($input));
 }
 
-class ChannelsDVR extends \App\SupportedApps implements \App\EnhancedApps {
+class ChannelsDVR extends \App\SupportedApps implements \App\EnhancedApps
+{
+	public $config;
 
-    public $config;
+	function __construct()
+	{
+	}
 
+	public function test()
+	{
+		$test = parent::appTest($this->url("dvr"));
+		echo $test->status;
+	}
 
-    function __construct() {
-    }
+	public function livestats()
+	{
+		$status = "inactive";
+		$res = parent::execute($this->url("dvr"));
+		$details = json_decode($res->getBody(), true);
 
-    public function test()
-    {
-        $test = parent::appTest($this->url('dvr'));
-        echo $test->status;
-    }
+		$data = [];
 
-    public function livestats()
-    {
-        $status = 'inactive';
-        $res = parent::execute($this->url('dvr'));
-        $details = json_decode($res->getBody(), true);
+		$activity = $details["activity"];
+		$recordings = count(preg_grep_keys("/^0-job-/", $activity));
+		$streams = count(preg_grep_keys("/^6-stream-/", $activity));
+		$data["recordings"] = number_format($recordings);
+		$data["streams"] = number_format($streams);
+		$status = "active";
 
-        $data = [];
-
-        $activity = $details['activity'];
-        $recordings = count( preg_grep_keys( "/^0-job-/", $activity ) );
-        $streams = count( preg_grep_keys( "/^6-stream-/", $activity ) );
-        $data['recordings'] = number_format($recordings);
-        $data['streams'] = number_format($streams);
-        $status = 'active';
-
-        return parent::getLiveStats($status, $data);
-
-    }
-    public function url($endpoint)
-    {
-        $api_url = parent::normaliseurl($this->config->url).$endpoint;
-        return $api_url;
-    }
+		return parent::getLiveStats($status, $data);
+	}
+	public function url($endpoint)
+	{
+		$api_url = parent::normaliseurl($this->config->url) . $endpoint;
+		return $api_url;
+	}
 }
--- ChannelsDVR/livestats.blade.php
+++ ChannelsDVR/livestats.blade.php
@@ -1,10 +1,10 @@
 <ul class="livestats">
-    <li>
-        <span class="title">Streams</span>
-        <strong>{!! $streams !!}</strong>
-    </li>
-    <li>
-        <span class="title">Recording</span>
-        <strong>{!! $recordings !!}</strong>
-    </li>
+	<li>
+		<span class="title">Streams</span>
+		<strong>{!! $streams !!}</strong>
+	</li>
+	<li>
+		<span class="title">Recording</span>
+		<strong>{!! $recordings !!}</strong>
+	</li>
 </ul>
