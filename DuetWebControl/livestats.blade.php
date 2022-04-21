032378c48b97c2477c58596af7e8b59f9511dbaf
DorjeDorf <45540765+DorjeDorf@users.noreply.github.com>
2022-04-22 00:21:28 +0200

    DuetWebControl enhanced app (#350)
    * update logo
    
    * tidy
    
    Co-authored-by: Daniel Gibbs <me@danielgibbs.co.uk>

---
+++ DuetWebControl/livestats.blade.php
@@ -0,0 +1,10 @@
+<ul class="livestats">
+    <li>
+        <span class="title">Status</span>
+        <strong>{!! $printer_status !!}</strong>
+    </li>
+    <li>
+        <span class="title">Progress</span>
+        <strong>{!! $job_progress !!}</strong>
+    </li>
+</ul>
