33c1627f0d309d2a6e8645d663d99088f552cf6f
Daniel Gibbs <me@danielgibbs.co.uk>
2022-07-27 22:24:01 +0100

    formatting

--- Plex/Plex.php
+++ Plex/Plex.php
@@ -18,58 +18,74 @@ class Plex extends \App\SupportedApps implements \App\EnhancedApps
 		echo $test->status;
 	}
 
-    public function test()
-    {
-        $test = parent::appTest($this->url('/library/recentlyAdded'), $this->attrs());
-        echo $test->status;
-    }
+	public function test()
+	{
+		$test = parent::appTest(
+			$this->url("/library/recentlyAdded"),
+			$this->attrs()
+		);
+		echo $test->status;
+	}
 
-    public function livestats()
-    {
-        $status = 'inactive';
-        $res = parent::execute($this->url('/library/recentlyAdded'), $this->attrs());
+	public function livestats()
+	{
+		$status = "inactive";
+		$res = parent::execute(
+			$this->url("/library/recentlyAdded"),
+			$this->attrs()
+		);
 
 		$res = parent::execute($this->url("/library/onDeck"));
 
-        $res = parent::execute($this->url('/library/onDeck'), $this->attrs());
+		$res = parent::execute($this->url("/library/onDeck"), $this->attrs());
 
-        $body = $res->getBody();
-        $xml = simplexml_load_string($body, 'SimpleXMLElement', LIBXML_NOCDATA | LIBXML_NOBLANKS);
-        if($xml) {
-            $data['on_deck'] = $xml['size'];
-            $status = 'active';
-        }
+		$body = $res->getBody();
+		$xml = simplexml_load_string(
+			$body,
+			"SimpleXMLElement",
+			LIBXML_NOCDATA | LIBXML_NOBLANKS
+		);
+		if ($xml) {
+			$data["on_deck"] = $xml["size"];
+			$status = "active";
+		}
 
-        return parent::getLiveStats($status, $data);
+		return parent::getLiveStats($status, $data);
+	}
+	public function url($endpoint)
+	{
+		$url = parse_url(parent::normaliseurl($this->config->url));
+		$scheme = $url["scheme"];
+		$domain = $url["host"];
+		$port = isset($url["port"]) ? $url["port"] : "32400";
+		$api_url =
+			$scheme .
+			"://" .
+			$domain .
+			":" .
+			$port .
+			$endpoint .
+			"?X-Plex-Token=" .
+			$this->config->token;
+		return $api_url;
+	}
 
-    }
-    public function url($endpoint)
-    {
-        $url = parse_url(parent::normaliseurl($this->config->url));
-        $scheme = $url['scheme'];
-        $domain = $url['host'];
-        $port = isset($url['port']) ? $url['port'] : '32400';
-        $api_url = $scheme."://".$domain.":".$port.$endpoint."?X-Plex-Token=".$this->config->token;
-        return $api_url;
+	public function getConfigValue($key, $default = null)
+	{
+		return isset($this->config) && isset($this->config->$key)
+			? $this->config->$key
+			: $default;
+	}
 
-    }
+	public function attrs()
+	{
+		$ignoreTls = $this->getConfigValue("ignore_tls", false);
+		if ($ignoreTls) {
+			$attrs["verify"] = false;
+		} else {
+			$attrs = [];
+		}
 
-    public function getConfigValue($key, $default=null)
-    {
-        return (isset($this->config) && isset($this->config->$key)) ? $this->config->$key : $default;
-    }
-
-
-    public function attrs()
-    {
-        $ignoreTls = $this->getConfigValue('ignore_tls', false);
-        if($ignoreTls) {
-            $attrs['verify'] = false;
-        }
-        else {
-            $attrs = [];
-        }
-
-        return $attrs;
-    }
+		return $attrs;
+	}
 }
