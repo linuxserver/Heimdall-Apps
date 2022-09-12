82ee8c0e5c852e743c774dfee27b5890f54426a9
Alacard <gamezol@gmail.com>
2022-09-11 20:59:24 -0500

    Add files via upload
    Changed from using Fever API to Google Reader compatible API

--- FreshRSS/FreshRSS.php
+++ FreshRSS/FreshRSS.php
@@ -18,41 +18,28 @@ class FreshRSS extends \App\SupportedApps implements \App\EnhancedApps
 	public function test()
 	{
 		try{
-			if($this->freshrssApiData("api/fever.php?api")->getStatusCode() != 200) throw new Exception();
-			$data_error = null;
-			$data_category = null;
-			$data = json_decode($this->freshrssApiData("api/fever.php?api&groups")->getBody());
-			if($data != null && $data->auth === 0) $data_error = "Error: Invalid Username and/or API key, Make sure your using the 'API password' and not the 'Authentication token'";
-			elseif($this->config->category !=null && $this->config->category !=""){
-				foreach($data->groups as $dataInner){
-					if(strtolower($dataInner->title) == strtolower($this->config->category)) $data_category = true;
+			$body=$this->freshrssApiData("login","api/greader.php/accounts/ClientLogin");
+			if($body==null) throw new \Exception("Error: Please check URL");
+			if($body->getStatusCode() == 401) throw new \Exception("Error: Invalid Username and/or API key, Make sure your using the 'API password' and not the 'Authentication token'");
+			if($body->getStatusCode() == 200){
+				if($this->config->category !=null && $this->config->category !=""){
+					$data = $body->getBody();
+					$data_auth=substr($data,(strpos($data,"Auth=")+5),strlen($data));
+					$data = json_decode($this->freshrssApiData("api","api/greader.php/reader/api/0/subscription/list?output=json",$data_auth)->getBody());
+					$data_category=false;
+					
+					foreach($data->subscriptions as $dataInner){
+						if(strtolower($dataInner->title) == strtolower($this->config->category)) $data_category = true;
+					}
+					
+					if(!$data_category) throw new \Exception("Error: Could not find catagory '" . $this->config->category . "'. Please make sure catagory is spelled correctly");
 				}
 				
-				if(!$data_category) $data_error = "Error: Could not find catagory '" . $this->config->category . "'. Please make sure catagory is spelled correctly";
+				echo "Welcome " . $this->config->username . ", you are connected to API";
 			}
-			if($data != null && $data->auth === 1 && $data_error == null) $data_error = "Welcome " . $this->config->username . ", you are connected to API";
-			echo $data_error;		
+			else echo "Error something went wrong";
 		}
-		catch(\Throwable $e){echo "Error: Please check URL";}
-	}
-
-	public function freshrssApiData($dataUrl)
-	{
-		$attrs = [
-			"body" => "api_key=" . $this->getApiKey(),
-			"headers" => [
-				"Content-Type" => "application/x-www-form-urlencoded",
-			],
-		];
-		
-		$res = parent::execute(
-			$this->url($dataUrl),
-			$attrs,
-			$this->clientVars,
-			"POST"
-		);
-
-		return $res;
+		catch(\Throwable $e){echo $e->getMessage();}
 	}
 
 	public function livestats()
@@ -60,117 +47,124 @@ class FreshRSS extends \App\SupportedApps implements \App\EnhancedApps
 		$status = "inactive";
 		$data = [];
 		$data["unread"] = null;
-		$data["embed"] = null;
-		$data["category"] = 'null';
-		
+		$data["embed"] = false;
+		$data["category"] = null;
+		$data["error"] = false;
+		$data["feed"] = null;
+		$data["id"] = null;
+
 		try{
-			if($this->freshrssApiData("api/fever.php?api")->getStatusCode() != 200) throw new Exception();	
-			if($this->config->embedded =='' || $this->config->embedded == null) throw new Exception();
-			if(filter_var($this->config->embedded, FILTER_VALIDATE_BOOLEAN)){
-			$data["embed"]=true;
-				
-				if ($this->config->category =='' || $this->config->category == null) {
+			$body=$this->freshrssApiData("login","api/greader.php/accounts/ClientLogin");
+			if($body==null) throw new \Exception();
+			if($body->getStatusCode() == 401) throw new \Exception();
+			if($body->getStatusCode() == 200){
+				$response = $body->getBody();
+					if($response==null) throw new \Exception();
+				$data["auth"]=substr($response,(strpos($response,"Auth=")+5),strlen($response));
+
+				if(filter_var($this->config->embedded, FILTER_VALIDATE_BOOLEAN)){
+					$data["embed"]=true;
 					
-				$body = json_decode($this->freshrssApiData("api/fever.php?api&unread_item_ids")->getBody());
-				
-					if ($body->auth === 1) {
+					if($this->config->category !=null && $this->config->category !=""){
 						
-						if ($body->unread_item_ids != ""){
-							if(count(explode(",", $body->unread_item_ids)) <= 10) $data["feedIds"] = $body->unread_item_ids; else{
-								$data["feedIdArray"]=explode(",", $body->unread_item_ids,11);
-								$trash=array_pop($data["feedIdArray"]);
-								$data["feedIds"]=implode(",",$data["feedIdArray"]);
+						$data["category"]=$this->config->category;
+						
+						$response = json_decode($this->freshrssApiData("api","api/greader.php/reader/api/0/subscription/list?output=json",$data["auth"])->getBody());
+							if($response==null) throw new \Exception();
+							
+							foreach($response->subscriptions as $responseInner){
+								if(strtolower($responseInner->title) == strtolower($data["category"])) $data["id"] = $responseInner->id;
 							}
-						$body = json_decode($this->freshrssApiData("api/fever.php?api&items&with_ids=" . $data["feedIds"])->getBody()); 
-						$data["feed"]=array();
-						$data["feedTotal"]=$body->total_items;
+							
+						if($data["id"]==null) throw new \Exception();
+							
+						$response = json_decode($this->freshrssApiData("api","api/greader.php/reader/api/0/stream/contents/" . $responseInner->id . "?output=json&n=10",$data["auth"])->getBody());
+							if($response==null) throw new \Exception();
 						$i=0;
-							foreach($body->items as $itemInner){
-									
-							$data["feed"][$i]=[];
-							$data["feed"][$i]['title']=$itemInner->title;
-							$data["feed"][$i]['body']=$itemInner->html;
-							$data["feed"][$i]['link']=$itemInner->url;
+						$data["feed"]=array();
+							foreach($response->items as $responseInner){
+								$data["feed"][$i]=[];
+								$data["feed"][$i]["title"]=$responseInner -> title;
+								$data["feed"][$i]["url"]=$responseInner -> canonical[0] -> href;
 							$i++;
 							}
-						}
-					}
-				}
-				else{
-					$data["category"] = $this->config->category;
-					$body = json_decode($this->freshrssApiData("api/fever.php?api&groups")->getBody());
-					
-					foreach($body->groups as $dataInner){
-						if(strtolower($dataInner->title) == strtolower($this->config->category)) $data["categoryid"] = $dataInner->id;
-					}
-					
-					$data["feed"]=array();
-					
-					if($data["categoryid"] !=null){
+
 						
-						if ($body->auth === 1){
-							foreach($body->groups as $dataInner){
-								if(strtolower($dataInner->title) == strtolower($this->config->category)) $data["categoryid"] = $dataInner->id;
-							}	
-							
-						$body = json_decode($this->freshrssApiData("api/fever.php?fever.php?api&items&group_id=" . $data['categoryid'])->getBody());
-							
-							if ($body->auth === 1) {
-								
-							$i=0;
-								foreach(array_slice($body->items, 0, 10 ) as $itemInner){
+					}
+					else{
+
+						$response = json_decode($this->freshrssApiData("api","api/greader.php/reader/api/0/stream/contents/user/-/state/com.google/reading-list?output=json&n=10",$data["auth"])->getBody());
+							if($response==null) throw new \Exception();
+						$i=0;
+						$data["feed"]=array();
+							foreach($response->items as $responseInner){
 								$data["feed"][$i]=[];
-								$data["feed"][$i]['title']=$itemInner->title;
-								$data["feed"][$i]['body']=$itemInner->html;
-								$data["feed"][$i]['link']=$itemInner->url;
-								$i++;
-								}								
-							}
-							else throw new Exception();
-						}
-						else throw new Exception();
-				
+								$data["feed"][$i]["title"]=$responseInner -> title;
+								$data["feed"][$i]["url"]=$responseInner -> canonical[0] -> href;
+							$i++;
+							}				
 					}
-					else throw new Exception();					
-				}
-			}
-			else{
-				if ($this->config->category =='' || $this->config->category == null) {
-					
-				$body = json_decode($this->freshrssApiData("api/fever.php?api&unread_item_ids")->getBody());
-				
-					if ($body->auth === 1) {
-						if ($body->unread_item_ids != "") $data["unread"] = count(explode(",", $body->unread_item_ids)) . " Unread"; else $data["unread"] = 0 . " Unread";
-					}
-					else throw new Exception();
 				}
 				else{
-					$data["category"] = $this->config->category;
+					$response = json_decode($this->freshrssApiData("api","api/greader.php/reader/api/0/unread-count?output=json",$data["auth"])->getBody());
 					
-					if($this->freshrssApiData("api/fever.php?api&groups")->getStatusCode() != 200) throw new Exception();
-					
-					$body = json_decode($this->freshrssApiData("api/fever.php?api&groups")->getBody());
-					
-					foreach($body->groups as $dataInner){
-						if(strtolower($dataInner->title) == strtolower($this->config->category)) $data["categoryid"] = $dataInner->id;
-					}
-
-					if($data["categoryid"] !=null){
+					if($this->config->category !=null && $this->config->category !=""){
 						
-					$body = json_decode($this->freshrssApiData("api/fever.php?api&mark=item&as=unread&id=" . $data['categoryid'])->getBody());
-					
-						if ($body->auth === 1) {
-							if ($body->unread_item_ids != "") $data["unread"] = count(explode(",", $body->unread_item_ids)) . " Unread"; else $data["unread"] = 0 . " Unread";
-						}
-						else throw new Exception();				
+						$data["category"]=$this->config->category;
+						
+							foreach($response->unreadcounts as $responseInner){
+								$data["id"]=substr(strtolower($responseInner->id),13,strlen($responseInner->id));
+								if($data["id"] == strtolower($data["category"])) $data["unread"] = $responseInner->count;
+							}						
+							
+							if($data["unread"]==null) throw new \Exception();
 					}
-					else throw new Exception();
+					else $data["unread"] = $response->max;
 				}
-			}
+			}			
 		}
-		catch(\Throwable $e){$data["unread"] = "Error";}
-
+		catch(\Throwable $e){$data["embed"]=true;}
+		
 		return parent::getLiveStats($status, $data);
+	}
+
+	public function freshrssApiData($dataMode,$dataUrl,$dataAuth=null)
+	{
+		if($dataMode=="login"){
+			
+			$attrs = [
+				"headers" => [
+					"Content-Type" => "application/x-www-form-urlencoded",
+				],			
+				"form_params" =>[
+					"Email" => $this->config->username,
+					"Passwd" => $this->config->apikey
+				],
+			];
+			
+			$res = parent::execute(
+				$this->url($dataUrl),
+				$attrs,
+				$this->clientVars,
+				"POST"
+			);
+
+			return $res;
+		}
+		elseif($dataMode=="api"){
+			$attrs = [
+				"headers" => ["Authorization" => "GoogleLogin auth=" . $dataAuth,],
+			];
+			
+			$res = parent::execute(
+				$this->url($dataUrl),
+				$attrs,
+				$this->clientVars,
+				"POST"
+			);
+
+			return $res;			
+		}
 	}
 
 	public function url($endpoint)
@@ -179,8 +173,4 @@ class FreshRSS extends \App\SupportedApps implements \App\EnhancedApps
 		return $api_url;
 	}
 
-	public function getApiKey()
-	{
-		return md5($this->config->username . ":" . $this->config->apikey);
-	}
 }
--- FreshRSS/livestats.blade.php
+++ FreshRSS/livestats.blade.php
@@ -1,35 +1,44 @@
-@if ($embed == null)
+@if (!$embed)
 <ul id="rssfeeds" class="livestats">
     <li>
         <span class="title" style="color:orange;">{!! $unread !!} </span>
     </li>
 </ul>
 @endif
-@if ($embed != null)
+
+@if ($embed)
 <div id="rssfeeds" class="rssfeed" style="position: relative;left: -90px;top: 35px;">
-	<div style="position: fixed;overflow: hidden;">
+	<div id="feedbox" style="position: fixed;overflow: hidden;">
 		<div style="display: inline-block;background-color: #161b1f;width: 279px;background-image: linear-gradient(90deg, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0.25));border-radius: 0px 0px 6px 6px;">
 		@foreach ($feed as $feedEntry)
-		<ul style="margin-left: -25px;width: 295px;overflow: hidden;">
-			<li style="list-style-type:none;">
-			<a href='{!! $feedEntry["link"] !!}' target="_blank" rel="noopener noreferrer" style="background-color: transparent;text-decoration: none !important;">
-				<span class="title" style="color:orange !important;white-space:nowrap;">{!! $feedEntry["title"] !!} </span>
-			</a>
-			</li>
-		</ul>
+			<ul style="margin-left: -25px;width: 295px;overflow: hidden;">
+				<li style="list-style-type:none;">
+				<a href='{!! $feedEntry["url"] !!}' target="_blank" rel="noopener noreferrer" style="background-color: transparent;text-decoration: none !important;">
+					<span class="title" style="color:orange !important;white-space:nowrap;">{!! $feedEntry["title"] !!} </span>
+				</a>
+				</li>
+			</ul>
 		@endforeach
 	</div>
 </div>
 @endif
+
 <script>
 @if ($category != null)
 $("#rssfeeds").parents("div.details").children("div.title.white").html("{!! $category !!}");	
-$("#rssfeeds").parents("div.details").children("div.title.white").css("width","141px");
+$("#rssfeeds").parents("div.details").children("div.title.white").css("width","140px");
 $("#rssfeeds").parents("div.details").children("div.title.white").css("white-space","nowrap");
 $("#rssfeeds").parents("div.details").children("div.title.white").css("overflow","hidden");
 @endif
-@if ($embed != null)
+
+@if ($embed)
 $("#rssfeeds").parents("div.item").css( "border-radius", "6px 6px 0px 0px" );
+$("#rssfeeds").parents("section.item-container.ui-sortable-handle").css( "height", "468px" );
+$("div.item").css( "height", "92px" );
+$("div#sortable").css( "align-items", "baseline");
+$("div#feedbox::after").css( "content", "");
+$("#rssfeeds").parents("section.item-container.ui-sortable-handle").children("a.item-edit").css("top","83px");
+$("#rssfeeds").parents("div.ui-sortable").children("section.add-item.ui-sortable-handle").css("top","-24px");
 @endif
 </script>
 
