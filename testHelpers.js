230d61554fcf7f36859204d1896272abd6bddad5
Attila Kerekes <ak@enrg8.com>
2022-11-16 15:29:59 +0100

    test: Add PNG Icon size tests

--- testHelpers.js
+++ testHelpers.js
@@ -1,7 +1,19 @@
-const { readFileSync, existsSync } = require("fs");
+const {
+	readFileSync,
+	existsSync,
+	openSync,
+	closeSync,
+	readSync,
+} = require("fs");
 
 const test = (description, callback) => {
-	if (!callback()) {
+	try {
+		if (!callback()) {
+			console.log(`\nTesting ${global.appUnderTest}`);
+			console.log(`Failed: ${description}`);
+			global.passed = false;
+		}
+	} catch (e) {
 		console.log(`\nTesting ${global.appUnderTest}`);
 		console.log(`Failed: ${description}`);
 		global.passed = false;
@@ -17,7 +29,34 @@ const getAppJson = (appDirectory) => {
 	return JSON.parse(readFileSync(appJsonPath).toString());
 };
 
+const getIconSizePNG = (fileName) => {
+	const HEADER_LENGTH = 24;
+	const OFFSET = 16;
+
+	if (!existsSync(fileName)) {
+		return { width: 0, height: 0 };
+	}
+
+	const fileDescriptor = openSync(fileName, "r");
+	const myBuffer = Buffer.alloc(HEADER_LENGTH);
+
+	try {
+		readSync(fileDescriptor, myBuffer, 0, HEADER_LENGTH, 0);
+
+		closeSync(fileDescriptor);
+	} catch (e) {
+		closeSync(fileDescriptor);
+		return { width: 0, height: 0 };
+	}
+
+	return {
+		width: myBuffer.readUInt32BE(OFFSET),
+		height: myBuffer.readUInt32BE(4 + OFFSET),
+	};
+};
+
 module.exports = {
 	test,
 	getAppJson,
+	getIconSizePNG,
 };
