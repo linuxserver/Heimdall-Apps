1dcff1b9911745a5851e05ced93656ebf3b64f83
dairidong <48778797+dairidong@users.noreply.github.com>
2022-12-03 06:00:32 +0800

    fix qBittorrent test (#567)
    Version api test should use cookies from login.

--- qBittorrent/qBittorrent.php
+++ qBittorrent/qBittorrent.php
@@ -19,11 +19,12 @@ class qBittorrent extends \App\SupportedApps implements \App\EnhancedApps
 	public function test()
 	{
 		$test = $this->login();
-		if ($test->getStatusCode() === 200) {
-			echo $test->getStatusCode();
-		}
+		$attrs = [
+            "cookies" => $this->jar,
+        ];
+		
 		#$test = parent::appTest($this->url('version/api'));
-		$test = parent::appTest($this->url("api/v2/app/version"));
+		$test = parent::appTest($this->url("api/v2/app/version"), $attrs);
 		echo $test->status;
 	}
 
