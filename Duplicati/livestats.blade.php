83db576208ed3e3f43a3dcfea749d1b412c18ec8
Jesse Connop <jconnop@gmail.com>
2022-12-15 10:42:19 +1300

    Upgrade Duplicati to enhanced app (#572)

---
+++ Duplicati/livestats.blade.php
@@ -0,0 +1,10 @@
+<ul class="livestats">
+    <li>
+        <span class="title">Next</span>
+        <strong>{!! $nextBackup !!}</strong>
+    </li>
+    <li>
+        <span class="title">Errors</span>
+        <strong>{!! $error !!}</strong>
+    </li>
+</ul>
