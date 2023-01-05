551e4e277aaebbb002b7bc49fc9ebdbf11b1475e
Juzzed <themultijuce@gmail.com>
2023-01-05 01:20:50 +0100

    Add valetudo enhanced app

---
+++ Valetudo/livestats.blade.php
@@ -0,0 +1,10 @@
+<ul class="livestats">
+    <li>
+        <span class="title">Last Session Time</span>
+        <strong>{!! $last_session_time !!}m</strong>
+    </li>
+    <li>
+        <span class="title">Last Session Area</span>
+        <strong>{!! $last_session_area !!}m²</strong>
+    </li>
+</ul>
