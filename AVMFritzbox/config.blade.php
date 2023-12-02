d0c189ebf9020b86b5a380464758354167987027
Michael Schmitz <michael+work@familyschmitz.de>
2023-12-02 12:45:17 +0100

    add override url, allows to set reverse proxied urls (#660)

--- AVMFritzbox/config.blade.php
+++ AVMFritzbox/config.blade.php
@@ -1,6 +1,10 @@
 <h2>{{ __('app.apps.config') }} ({{ __('app.optional') }}) @include('items.enable')</h2>
 <div class="items">
     <div class="input">
+        <label>{{ strtoupper(__('app.url')) }}</label>
+        {!! Form::text('config[override_url]', isset($item) ? $item->getconfig()->override_url : null, ['placeholder' => __('app.apps.override'), 'id' => 'override_url', 'class' => 'form-control']) !!}
+    </div>
+    <div class="input">
         <label>Stats to show</label>
         {!! Form::select('config[availablestats][]', App\SupportedApps\AVMFritzbox\AVMFritzbox::getAvailableStats(), isset($item) ? $item->getConfig()->availablestats ?? null : null, ['multiple' => 'multiple']) !!}
     </div>
