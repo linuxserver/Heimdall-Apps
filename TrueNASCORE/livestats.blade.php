f136026bebf70a84371a1d81e131c02c5ecd843b
Sander <scebbers91@gmail.com>
2024-01-14 11:48:08 +0100

    Update livestats.blade.php

--- TrueNASCORE/livestats.blade.php
+++ TrueNASCORE/livestats.blade.php
@@ -5,6 +5,6 @@
     </li>
     <li>
         <span class="title">Alerts</span>
-        <strong>{!! $alert_tot !!}</strong>
+        <strong>{!! $alert_tot !!}  / <span style="background-color: #d64d55; padding: 3px; color: white; font-size: 12px; border-radius: 3px; opacity: 1;">{!! $alert_crit !!}</span></strong>
     </li>
 </ul>
