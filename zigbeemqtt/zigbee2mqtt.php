4c53065d4ec26d7fc63331c30af70843d8ff27ca
Martijn <martijn.niji@gmail.com>
2024-01-24 14:29:00 +0100

    Update and rename zigbeemqtt.php to zigbee2mqtt.php

---
+++ zigbeemqtt/zigbee2mqtt.php
@@ -0,0 +1,5 @@
+<?php namespace App\SupportedApps\zigbee2mqtt;
+
+class zigbee2mqtt extends \App\SupportedApps {
+
+}
--- zigbeemqtt/zigbeemqtt.php
+++
@@ -1,5 +0,0 @@
-<?php namespace App\SupportedApps\zigbeemqtt;
-
-class zigbeemqtt extends \App\SupportedApps {
-
-}
