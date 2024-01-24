e8a5e5871889f7e13b3ec322b00c2fc4b0faeae5
Jaromír Staněk <jaromir.stanek@gmail.com>
2024-01-24 13:13:51 +0100

    Add GNS3 enhanced app (#647)
    * Added template for enhanced app.
    * Enhanced app implementation for GNS3.
    * Minified png image.

---
+++ GNS3/GNS3.php
@@ -0,0 +1,51 @@
+<?php namespace App\SupportedApps\GNS3;
+
+class GNS3 extends \App\SupportedApps implements \App\EnhancedApps {
+
+    public $config;
+
+    //protected $login_first = true; // Uncomment if api requests need to be authed first
+    //protected $method = 'POST';  // Uncomment if requests to the API should be set by POST
+
+    function __construct() {
+        //$this->jar = new \GuzzleHttp\Cookie\CookieJar; // Uncomment if cookies need to be set
+    }
+
+    public function test()
+    {
+        $test = parent::appTest($this->url('/v2/version'), ['auth' => [$this->config->username, $this->config->password]]);
+        $details = json_decode($test->response);
+        if ($details && isset($details->version)) {
+            echo $test->status . "\nServer version: " . $details->version;
+        } else {
+            echo $test->status;
+        }
+    }
+
+    public function livestats()
+    {
+        $status = 'inactive';
+        $res = parent::execute($this->url('/v2/projects'), ['auth' => [$this->config->username, $this->config->password]]);
+        $details = json_decode($res->getBody());
+
+        $data = [];
+        $data["opened"] = 0;
+        $data["closed"] = 0;
+        
+        foreach ($details as $project) {
+            if ($project->status == "opened") {
+                $data["opened"]++;
+            } else {
+                $data["closed"]++;
+            }
+        }
+        
+        return parent::getLiveStats($status, $data);
+        
+    }
+    public function url($endpoint)
+    {
+        $api_url = parent::normaliseurl($this->config->url, false).$endpoint;
+        return $api_url;
+    }
+}
---
+++ GNS3/livestats.blade.php
@@ -0,0 +1,10 @@
+<ul class="livestats">
+    <li>
+        <span class="title">Opened</span>
+        <strong>{!! $opened !!}</strong>
+    </li>
+    <li>
+        <span class="title">Closed</span>
+        <strong>{!! $closed !!}</strong>
+    </li>
+</ul>
