f5849a51c6b22db3120b7b268f0aa3e8ca6b2684
Listiges Känguru <48389318+listiges-kaenguru@users.noreply.github.com>
2024-02-22 14:16:06 +0100

    Add files for enhanced features

---
+++ TrueNASSCALE/livestats.blade.php
@@ -0,0 +1,10 @@
+<ul class="livestats">
+    <li>
+        <span class="title">Uptime</span>
+        <strong>{!! $uptime !!}</strong>
+    </li>
+    <li>
+        <span class="title">Alerts</span>
+        <strong>{!! $alert_tot !!}  / <span style="background-color: #d64d55; padding: 3px; color: white; font-size: 12px; border-radius: 3px; opacity: 1;">{!! $alert_crit !!}</span></strong>
+    </li>
+</ul>
