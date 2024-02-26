74cd7ac3d72047e47d774f198033f5a317b6f259
James Ward <james@notjam.es>
2024-02-26 00:19:26 -0500

    feat: enhance scrutiny foundation app

--- Scrutiny/Scrutiny.php
+++ Scrutiny/Scrutiny.php
@@ -2,6 +2,62 @@
 
 namespace App\SupportedApps\Scrutiny;
 
-class Scrutiny extends \App\SupportedApps
+class Scrutiny extends \App\SupportedApps implements \App\EnhancedApps
 {
+	public $config;
+
+	public function __construct()
+	{
+	}
+
+	public function test()
+	{
+		$attrs = [
+			"headers" => ["Accept" => "application/json"],
+		];
+		$test = parent::appTest($this->url("health"), $attrs);
+		echo $test->status;
+	}
+
+	public function livestats()
+	{
+		$status = "inactive";
+		$data = [
+			"passed" => 0,
+			"failed" => 0,
+		];
+
+		$attrs = [
+			"headers" => ["Accept" => "application/json"],
+		];
+
+		$response = json_decode(
+			parent::execute($this->url("summary"), $attrs)->getBody()
+		);
+
+		$summary = $response->data->summary;
+
+		foreach ($summary as $entry) {
+			$device_status = $entry->device->device_status;
+
+			if ($device_status == 0) {
+				$data["passed"]++;
+			} else {
+				$status = "active";
+				$data["failed"]++;
+			}
+		}
+
+		return parent::getLiveStats($status, $data);
+	}
+
+	public function url($endpoint)
+	{
+		$api_url =
+			parent::normaliseurl($this->config->url) .
+			"api/" .
+			$endpoint;
+
+		return $api_url;
+	}
 }
