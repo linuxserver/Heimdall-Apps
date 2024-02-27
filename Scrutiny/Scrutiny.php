a790c1a5570dfdfc48a181fe21e6626c2f534f08
James Ward <james@notjam.es>
2024-02-27 14:55:07 -0500

    chore: update scrutiny to use spaces rather than tabsuse spaces rather than tabs

--- Scrutiny/Scrutiny.php
+++ Scrutiny/Scrutiny.php
@@ -4,60 +4,60 @@ namespace App\SupportedApps\Scrutiny;
 
 class Scrutiny extends \App\SupportedApps implements \App\EnhancedApps
 {
-	public $config;
+    public $config;
 
-	public function __construct()
-	{
-	}
+    public function __construct()
+    {
+    }
 
-	public function test()
-	{
-		$attrs = [
-			"headers" => ["Accept" => "application/json"],
-		];
-		$test = parent::appTest($this->url("health"), $attrs);
-		echo $test->status;
-	}
+    public function test()
+    {
+        $attrs = [
+            "headers" => ["Accept" => "application/json"],
+        ];
+        $test = parent::appTest($this->url("health"), $attrs);
+        echo $test->status;
+    }
 
-	public function livestats()
-	{
-		$status = "inactive";
-		$data = [
-			"passed" => 0,
-			"failed" => 0,
-		];
+    public function livestats()
+    {
+        $status = "inactive";
+        $data = [
+            "passed" => 0,
+            "failed" => 0,
+        ];
 
-		$attrs = [
-			"headers" => ["Accept" => "application/json"],
-		];
+        $attrs = [
+            "headers" => ["Accept" => "application/json"],
+        ];
 
-		$response = json_decode(
-			parent::execute($this->url("summary"), $attrs)->getBody()
-		);
+        $response = json_decode(
+            parent::execute($this->url("summary"), $attrs)->getBody()
+        );
 
-		$summary = $response->data->summary;
+        $summary = $response->data->summary;
 
-		foreach ($summary as $entry) {
-			$device_status = $entry->device->device_status;
+        foreach ($summary as $entry) {
+            $device_status = $entry->device->device_status;
 
-			if ($device_status == 0) {
-				$data["passed"]++;
-			} else {
-				$status = "active";
-				$data["failed"]++;
-			}
-		}
+            if ($device_status == 0) {
+                $data["passed"]++;
+            } else {
+                $status = "active";
+                $data["failed"]++;
+            }
+        }
 
-		return parent::getLiveStats($status, $data);
-	}
+        return parent::getLiveStats($status, $data);
+    }
 
-	public function url($endpoint)
-	{
-		$api_url =
-			parent::normaliseurl($this->config->url) .
-			"api/" .
-			$endpoint;
+    public function url($endpoint)
+    {
+        $api_url =
+            parent::normaliseurl($this->config->url) .
+            "api/" .
+            $endpoint;
 
-		return $api_url;
-	}
+        return $api_url;
+    }
 }
