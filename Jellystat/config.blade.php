9862e03cfdde89ae5529ca5f1eef59d301bdd133
kekZiger <kekz.trust@gmail.com>
2024-05-17 17:19:14 +0200

    Overhaul...

--- Jellystat/config.blade.php
+++ Jellystat/config.blade.php
@@ -5,12 +5,17 @@
         {!! Form::text('config[override_url]', isset($item) ? $item->getconfig()->override_url : null, ['placeholder' => __('app.apps.override'), 'id' => 'override_url', 'class' => 'form-control']) !!}
     </div>
     <div class="input">
-        <label>{{ __('app.apps.x-api-token') }} (API token)</label>
-        {!! Form::input('x-api-token', 'config[x-api-token]', '', ['placeholder' => __('app.apps.x-api-token'), 'data-config' => 'x-api-token', 'class' => 'form-control config-item']) !!}
+        <label>API token</label>
+        {!! Form::input('text', 'config[x_api_token]', isset($item) ? $item->getconfig()->x_api_token : null, ['placeholder' => __('Api Token'), 'data-config' => 'x_api_token', 'class' => 'form-control config-item']) !!}
     </div>
     <div class="input">
         <label>Stats to show</label>
-        {!! Form::select('config[availablestats][]', App\SupportedApps\Jellyfin\Jellyfin::getAvailableStats(), isset($item) ? $item->getConfig()->availablestats ?? null : null, ['multiple' => 'multiple']) !!}
+        <!--{!! Form::select('config[availablestats][]', App\SupportedApps\Jellystat\Jellystat::getAvailableStats(), isset($item) ? $item->getConfig()->availablestats ?? null : null, ['multiple' => 'multiple']) !!} -->
+        {!! Form::select('config[availablestats][]', App\SupportedApps\Jellystat\Jellystat::getAvailableStats(), isset($item) ? $item->getConfig()->availablestats ?? null : null) !!}
+    </div>
+    <div class="input">
+        <label>Debug (1 == yes)</label>
+        {!! Form::select('config[debug][]', App\SupportedApps\Jellystat\Jellystat::getDebugStatus(), isset($item) ? $item->getconfig()->debug ?? null : null) !!}
     </div>
     <div class="input">
         <button style="margin-top: 32px;" class="btn test" id="test_config">Test</button>
--- Jellystat/livestats.blade.php
+++ Jellystat/livestats.blade.php
@@ -1,8 +1,12 @@
 <ul class="livestats">
     @foreach ($visiblestats as $stat)
-        <li>
-            <span class="title">{!! $stat->title !!}</span>
-            <strong>{!! $stat->value !!}</strong>
-        </li>
+    <li>
+        <span class="title">{!! $stat->TotalPlays->title !!}</span>
+        <strong>{!! $stat->TotalPlays->value !!}</strong>
+    </li>
+    <li>
+        <span class="title">{!! $stat->TotalWatchTime->title !!}</span>
+        <strong>{!! $stat->TotalWatchTime->value !!}</strong>
+    </li>
     @endforeach
 </ul>
