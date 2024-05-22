febd0302345fe5f5bd1d62d9c10713f0e732f51f
kekZiger <kekz.trust@gmail.com>
2024-05-22 17:10:35 +0200

    Changes for PHPCS

--- Audiobookshelf/Audiobookshelf.php
+++ Audiobookshelf/Audiobookshelf.php
@@ -4,7 +4,6 @@ namespace App\SupportedApps\Audiobookshelf;
 
 class Audiobookshelf extends \App\SupportedApps implements \App\EnhancedApps
 {
-
     public $config;
 
     //protected $login_first = true; // Uncomment if api requests need to be authed first
@@ -24,31 +23,17 @@ class Audiobookshelf extends \App\SupportedApps implements \App\EnhancedApps
     public function livestats()
     {
         $status = 'inactive';
-        #$res = parent::execute($this->url('api/users/root/listening-sessions'),$this->getAttrs(),NULL,"POST");
-        #$res = parent::execute($this->url('api/authorize'),$this->getAttrs(),NULL,"POST");
-        $res = parent::execute($this->url('api/me/listening-stats'),$this->getAttrs());
+        $res = parent::execute($this->url('api/me/listening-stats'), $this->getAttrs());
         $details = json_decode($res->getBody());
-
-        #echo '<pre>';
-       # print_r($res);
-       # print_r($details);
-
         $data = ['totalTime' => $this->secondsToHoursMinutes($details->totalTime)];
-
-
-       # echo '</pre>';
         return parent::getLiveStats($status, $data);
-
     }
 
     public function url($endpoint)
     {
-        $api_url = parent::normaliseurl($this->config->url).$endpoint;
+        $api_url = parent::normaliseurl($this->config->url) . $endpoint;
         return $api_url;
     }
-
-
-
 
     private function getAttrs()
     {
@@ -56,25 +41,17 @@ class Audiobookshelf extends \App\SupportedApps implements \App\EnhancedApps
             "headers" => [
                 "accept" => "application/json",
                 "Authorization" =>
-                    "Bearer " . $this->config->apikey
+                "Bearer " . $this->config->apikey
             ],
         ];
     }
 
 
-    function secondsToHoursMinutes($seconds) {
-
-        // Calculate the hours
+    private function secondsToHoursMinutes($seconds)
+    {
         $hours = floor($seconds / 3600);
-
-        // Calculate the remaining seconds
-        // into minutes
         $minutes = floor(($seconds % 3600) / 60);
-
         $return_seconds = floor($seconds % 60);
-
-        // Return the result as an
-        // associative array
-        return $hours . 'h ' . $minutes . 'min ' . $return_seconds. 'sec';
+        return $hours . 'h ' . $minutes . 'min ' . $return_seconds . 'sec';
     }
 }
