4382ef68a4cd4f7c117a51cf1907d48de28a4c98
kekZiger <kekz.trust@gmail.com>
2024-05-23 10:24:20 +0200

    Changed Audiobookshelf to enhanced (#735)
    * Added API Support
    Shows "Total Watch Time" of 1 User (identified by the apikey)
    
    * Optimize 1 SVG(s)
    
    * Changes for PHPCS
    
    ---------
    
    Co-authored-by: kekZiger <kekZiger@users.noreply.github.com>

--- Audiobookshelf/Audiobookshelf.php
+++ Audiobookshelf/Audiobookshelf.php
@@ -2,6 +2,56 @@
 
 namespace App\SupportedApps\Audiobookshelf;
 
-class Audiobookshelf extends \App\SupportedApps
+class Audiobookshelf extends \App\SupportedApps implements \App\EnhancedApps
 {
+    public $config;
+
+    //protected $login_first = true; // Uncomment if api requests need to be authed first
+    //protected $method = 'POST';  // Uncomment if requests to the API should be set by POST
+
+    public function __construct()
+    {
+        //$this->jar = new \GuzzleHttp\Cookie\CookieJar; // Uncomment if cookies need to be set
+    }
+
+    public function test()
+    {
+        $test = parent::appTest($this->url('status'));
+        echo $test->status;
+    }
+
+    public function livestats()
+    {
+        $status = 'inactive';
+        $res = parent::execute($this->url('api/me/listening-stats'), $this->getAttrs());
+        $details = json_decode($res->getBody());
+        $data = ['totalTime' => $this->secondsToHoursMinutes($details->totalTime)];
+        return parent::getLiveStats($status, $data);
+    }
+
+    public function url($endpoint)
+    {
+        $api_url = parent::normaliseurl($this->config->url) . $endpoint;
+        return $api_url;
+    }
+
+    private function getAttrs()
+    {
+        return [
+            "headers" => [
+                "accept" => "application/json",
+                "Authorization" =>
+                "Bearer " . $this->config->apikey
+            ],
+        ];
+    }
+
+
+    private function secondsToHoursMinutes($seconds)
+    {
+        $hours = floor($seconds / 3600);
+        $minutes = floor(($seconds % 3600) / 60);
+        $return_seconds = floor($seconds % 60);
+        return $hours . 'h ' . $minutes . 'min ' . $return_seconds . 'sec';
+    }
 }
