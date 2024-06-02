e12ba4825dd66aac68cfde94ada67e8cc70a65c6
Albin Medoc <stamline203@gmail.com>
2024-06-02 23:23:25 +0200

    Add Maintainerr Enhanced App

---
+++ Maintainerr/Maintainerr.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\SupportedApps\Maintainerr;
+
+class Maintainerr extends \App\SupportedApps implements \App\EnhancedApps
+{
+    public $config;
+
+    //protected $login_first = true; // Uncomment if api requests need to be authed first
+    //protected $method = 'POST';  // Uncomment if requests to the API should be set by POST
+
+    public function __construct()
+    {
+        //$this->jar = new \GuzzleHttp\Cookie\CookieJar; // Uncomment if cookies need to be set
+    }
+
+    public function test()
+    {
+        $test = parent::appTest($this->url(""));
+        echo $test->status;
+    }
+
+    public function livestats()
+    {
+        $status = "inactive";
+        $data = [];
+
+        $rules = json_decode(parent::execute($this->url("api/rules"))->getBody());
+        $activeRules = $filteredObjects = array_filter($rules, function ($rule) {
+            return isset($rule->isActive) && $rule->isActive;
+        });
+
+        $data["rules"] = count($activeRules);
+
+        return parent::getLiveStats($status, $data);
+    }
+
+    public function url($endpoint)
+    {
+        $api_url = parent::normaliseurl($this->config->url) . $endpoint;
+        return $api_url;
+    }
+}
---
+++ Maintainerr/livestats.blade.php
@@ -0,0 +1,6 @@
+<ul class="livestats">
+    <li>
+        <span class="title">Rules</span>
+        <strong>{!! $rules !!}</strong>
+    </li>
+</ul>
