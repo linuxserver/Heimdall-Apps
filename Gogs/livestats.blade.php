c7a4afa5f762f62c8dbe075316d6306dead52bf2
Albin Medoc <stamline203@gmail.com>
2024-06-02 17:16:23 +0200

    Make Gogs Enhanced App

---
+++ Gogs/livestats.blade.php
@@ -0,0 +1,10 @@
+<ul class="livestats">
+    <li>
+        <span class="title">Repos</span>
+        <strong>{!! $repositories !!}</strong>
+    </li>
+    <li>
+        <span class="title">Organizations</span>
+        <strong>{!! $organizations !!}</strong>
+    </li>
+</ul>
