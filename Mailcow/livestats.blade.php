c0ecde2df40f8ee7f1a250553645192591e7a041
Pedro Buffon <henriquebuffon@gmail.com>
2024-07-10 14:18:55 -0300

    Enchance Mailcow

---
+++ Mailcow/livestats.blade.php
@@ -0,0 +1,14 @@
+<ul class="livestats">
+    <li>
+        <span class="title">Mailboxes</span>
+        <strong>{!! $mailboxes !!}</strong>
+    </li>
+    <li>
+        <span class="title">Domains</span>
+        <strong>{!! $domains !!}</strong>
+    </li>
+    <li>
+        <span class="title">Queue</span>
+        <strong>{!! $queue !!}</strong>
+    </li>
+</ul>
