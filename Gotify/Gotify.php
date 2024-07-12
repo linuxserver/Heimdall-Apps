fb2a18e7fc7c64799ed701e1f69ffdea16ad375d
Pedro Buffon <henriquebuffon@gmail.com>
2024-07-12 10:20:58 -0300

    Remove whitespaces

--- Gotify/Gotify.php
+++ Gotify/Gotify.php
@@ -22,45 +22,45 @@ class Gotify extends \App\SupportedApps implements \App\EnhancedApps
         $status = "inactive";
         $data = [];
         $attrs = $this->getAttrs();
-    
+
         // Fetch applications
         $applicationsResponse = parent::execute($this->url("application"), $attrs);
         $applications = json_decode($applicationsResponse->getBody());
-    
+
         // Count applications
         if ($applications) {
             $data["applications"] = count($applications);
         } else {
             $data["applications"] = 0;
         }
-    
+
         // Fetch clients
         $clientsResponse = parent::execute($this->url("client"), $attrs);
         $clients = json_decode($clientsResponse->getBody());
-    
+
         // Count clients
         if ($clients) {
             $data["clients"] = count($clients);
         } else {
             $data["clients"] = 0;
         }
-    
+
         // Fetch messages
         $messagesResponse = parent::execute($this->url("message"), $attrs);
         $messages = json_decode($messagesResponse->getBody());
-    
+
         // Count messages
         if ($messages && isset($messages->messages)) {
             $data["messages"] = count($messages->messages);
         } else {
             $data["messages"] = 0;
         }
-    
+
         // Determine status based on data
         if ($data["applications"] > 0 || $data["clients"] > 0 || $data["messages"] > 0) {
             $status = "active";
         }
-    
+
         return parent::getLiveStats($status, $data);
     }
 
@@ -78,5 +78,4 @@ class Gotify extends \App\SupportedApps implements \App\EnhancedApps
             ],
         ];
     }
-
 }
