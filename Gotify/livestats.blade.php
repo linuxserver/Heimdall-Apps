c1708495806db98bf84706ed8d5b4e93656df56e
PedroBuffon <henriquebuffon@gmail.com>
2024-07-13 09:34:12 -0300

    Enhanced gotify (#756)
    * Enhancing Gotify App(Need Testing)
    
    * Enhancing Gotify tested and working
    
    * Remove whitespaces

---
+++ Gotify/livestats.blade.php
@@ -0,0 +1,14 @@
+<ul class="livestats">
+    <li>
+        <span class="title">Apps</span>
+        <strong>{!! $applications !!}</strong>
+    </li>
+    <li>
+        <span class="title">Clients</span>
+        <strong>{!! $clients !!}</strong>
+    </li>
+    <li>
+        <span class="title">Messages</span>
+        <strong>{!! $messages !!}</strong>
+    </li>
+</ul>
