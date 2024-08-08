ddfca57383249c65fceeeabb672ba01d8d507e4b
svanhoutte <22617546+svanhoutte@users.noreply.github.com>
2024-08-08 13:09:44 -0400

    Update PhotoPrism.php to support authentication

--- PhotoPrism/PhotoPrism.php
+++ PhotoPrism/PhotoPrism.php
@@ -1,44 +1,50 @@
-<?php
-
-namespace App\SupportedApps\PhotoPrism;
+<?php namespace App\SupportedApps\PhotoPrism;
 
 class PhotoPrism extends \App\SupportedApps implements \App\EnhancedApps
 {
-    public $config;
+	public $config;
 
-    //protected $login_first = true; // Uncomment if api requests need to be authed first
-    //protected $method = 'POST';  // Uncomment if requests to the API should be set by POST
+	//protected $login_first = true; // Uncomment if api requests need to be authed first
+	//protected $method = 'POST';  // Uncomment if requests to the API should be set by POST
 
-    public function __construct()
+	function __construct()
+	{
+		//$this->jar = new \GuzzleHttp\Cookie\CookieJar; // Uncomment if cookies need to be set
+	}
+
+	function get_request_attrs()
     {
-        //$this->jar = new \GuzzleHttp\Cookie\CookieJar; // Uncomment if cookies need to be set
+		$attrs["headers"] = ["X-Session-ID" => $this->config->session];
+        return $attrs;
     }
 
-    public function test()
-    {
-        $test = parent::appTest($this->url("api/v1/config"));
-        echo $test->status;
-    }
+	public function test()
+	{
+        $attrs = $this->get_request_attrs();
+		$test = parent::appTest($this->url("api/v1/config"), $attrs);
+		echo $test->status;
+	}
 
-    public function livestats()
-    {
-        $status = "inactive";
-        $res = parent::execute($this->url("api/v1/config"));
-        $details = json_decode($res->getBody(), true);
+	public function livestats()
+	{
+		$status = "inactive";
+		$attrs = $this->get_request_attrs();
+		$res = parent::execute($this->url("api/v1/config"), $attrs);
+		$details = json_decode($res->getBody(), true);
+		$data = [];
 
-        $data = [];
+		if ($details) {
+			$status = "active";
+			$data["photos"] = number_format($details["count"]["photos"]) ?? 0;
+			$data["videos"] = number_format($details["count"]["videos"]) ?? 0;
+		}
 
-        if ($details) {
-            $data["photos"] = number_format($details["count"]["photos"]) ?? 0;
-            $data["videos"] = number_format($details["count"]["videos"]) ?? 0;
-        }
+		return parent::getLiveStats($status, $data);
+	}
 
-        return parent::getLiveStats($status, $data);
-    }
-
-    public function url($endpoint)
-    {
-        $api_url = parent::normaliseurl($this->config->url) . $endpoint;
-        return $api_url;
-    }
+	public function url($endpoint)
+	{
+		$api_url = parent::normaliseurl($this->config->url) . $endpoint;
+		return $api_url;
+	}
 }
