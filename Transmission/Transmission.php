8255133c6014b8a99e6cec92e8c4bbf7997170d4
GOUKI9999 <gouki9999@hotmail.com>
2024-08-17 18:55:34 +0800

    Fix-for-Transmission4 (#765)
    * Fix-for-Transmission4
    
    * php-cs-fix

--- Transmission/Transmission.php
+++ Transmission/Transmission.php
@@ -8,12 +8,10 @@ class Transmission extends \App\SupportedApps implements \App\EnhancedApps
     public $attrs = [];
     public $vars;
 
-    //protected $login_first = true; // Uncomment if api requests need to be authed first
-    protected $method = "POST"; // Uncomment if requests to the API should be set by POST
+    protected $method = "POST";
 
     public function __construct()
     {
-        //$this->jar = new \GuzzleHttp\Cookie\CookieJar; // Uncomment if cookies need to be set
         $body["method"] = "torrent-get";
         $body["arguments"] = [
             "fields" => ["percentDone", "status", "rateDownload", "rateUpload"],
@@ -22,13 +20,13 @@ class Transmission extends \App\SupportedApps implements \App\EnhancedApps
             "http_errors" => false,
             "timeout" => 5,
             "body" => json_encode($body),
+            "verify" => false,
         ];
     }
 
     public function test()
     {
         $test = $this->sendTest();
-
         echo $test->status;
     }
 
@@ -37,17 +35,17 @@ class Transmission extends \App\SupportedApps implements \App\EnhancedApps
         $status = "inactive";
         $res = $this->sendRequest();
         if ($res == null) {
-            //Log::debug('Transmission connection failed');
             return "";
         }
 
         $details = json_decode($res->getBody());
         if (!isset($details->arguments)) {
-            //Log::debug('Failed to fetch data from Transmission');
             return "";
         }
 
         $data = [];
+
+        error_log(json_encode($details, JSON_PRETTY_PRINT));
 
         $torrents = $details->arguments->torrents;
         $seeding_torrents = 0;
@@ -69,18 +67,8 @@ class Transmission extends \App\SupportedApps implements \App\EnhancedApps
             $status = "active";
         }
 
-        $data["download_rate"] = format_bytes(
-            $rateDownload,
-            false,
-            " <span>",
-            "/s</span>"
-        );
-        $data["upload_rate"] = format_bytes(
-            $rateUpload,
-            false,
-            " <span>",
-            "/s</span>"
-        );
+        $data["download_rate"] = format_bytes($rateDownload, false, " <span>", "/s</span>");
+        $data["upload_rate"] = format_bytes($rateUpload, false, " <span>", "/s</span>");
         $data["seed_count"] = $seeding_torrents;
         $data["leech_count"] = $leeching_torrents;
 
@@ -90,18 +78,10 @@ class Transmission extends \App\SupportedApps implements \App\EnhancedApps
     private function sendTest()
     {
         $this->setClientOptions();
-        $test = parent::appTest(
-            $this->url("transmission/rpc"),
-            $this->attrs,
-            $this->vars
-        );
+        $test = parent::appTest($this->url("transmission/rpc"), $this->attrs, $this->vars);
         if ($test->code === 409) {
             $this->setClientOptions();
-            $test = parent::appTest(
-                $this->url("transmission/rpc"),
-                $this->attrs,
-                $this->vars
-            );
+            $test = parent::appTest($this->url("transmission/rpc"), $this->attrs, $this->vars);
         }
         return $test;
     }
@@ -109,18 +89,10 @@ class Transmission extends \App\SupportedApps implements \App\EnhancedApps
     private function sendRequest()
     {
         $this->setClientOptions();
-        $res = parent::execute(
-            $this->url("transmission/rpc"),
-            $this->attrs,
-            $this->vars
-        );
+        $res = parent::execute($this->url("transmission/rpc"), $this->attrs, $this->vars);
         if ($res->getStatusCode() === 409) {
             $this->setClientOptions();
-            $res = parent::execute(
-                $this->url("transmission/rpc"),
-                $this->attrs,
-                $this->vars
-            );
+            $res = parent::execute($this->url("transmission/rpc"), $this->attrs, $this->vars);
         }
         return $res;
     }
@@ -134,27 +106,23 @@ class Transmission extends \App\SupportedApps implements \App\EnhancedApps
                     $this->config->password,
                     "Basic",
                 ],
+                "verify" => false,
+            ];
+        } else {
+            $this->attrs = [
+                "verify" => false,
             ];
         }
-        $res = parent::execute(
-            $this->url("transmission/rpc"),
-            $this->attrs,
-            $this->vars
-        );
+        $res = parent::execute($this->url("transmission/rpc"), $this->attrs, $this->vars);
 
         try {
-            //print_r($res);
             $xtId = $res->getHeaderLine("X-Transmission-Session-Id");
             if ($xtId != null) {
                 $this->attrs["headers"] = [
                     "X-Transmission-Session-Id" => $xtId,
                 ];
-            } else {
-                //Log::error("Unable to get Transmission session information");
-                //Log::debug("Status Code: ".$res->getStatusCode());
             }
         } catch (\GuzzleHttp\Exception\ConnectException $e) {
-            //Log::error("Failed connection to Transmission");
             return false;
         }
         return true;
