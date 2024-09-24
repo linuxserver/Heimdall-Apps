3738a70b854d35f7467ae657343aa5b761c36559
Pedro Buffon <henriquebuffon@gmail.com>
2024-09-24 11:42:32 +0000

    refactored to work with v5 and v6

--- Pihole/Pihole.php
+++ Pihole/Pihole.php
@@ -18,39 +18,91 @@ class Pihole extends \App\SupportedApps implements \App\EnhancedApps
 
     public function test()
     {
-        $test = parent::appTest($this->url("api.php?summaryRaw"));
-        echo $test->status;
+        $version = $this->config->version;
+        if ($version == 5) {
+            $test = parent::appTest($this->url("api.php?summaryRaw"));
+            echo $test->status;
+        }
+        if ($version == 6) {
+            $test = $this->login();
+            if ($test->getStatusCode() === 200 && $test->getBody()) {
+                $wk_resp = json_decode($test->getBody());
+                echo "Hello " . $wk_resp->data->username;
+            } else {
+                echo "Failed";
+            }
+        }
     }
-
     public function livestats()
     {
         $status = "inactive";
-        $res = parent::execute($this->url("api.php?summaryRaw"));
-        $details = json_decode($res->getBody());
+        $version = $this->config->version;
+        
+        if ($version == 5) {
+            $res = parent::execute($this->url("api.php?summaryRaw"));
+            $details = json_decode($res->getBody());
 
-        $data = [];
+            $data = [];
 
-        if ($details) {
-            $data["ads_blocked_today"] = number_format(
-                $details->ads_blocked_today
-            );
-            $data["ads_percentage_today"] = number_format(
-                $details->ads_percentage_today,
-                1
-            );
+            if ($details) {
+                $data["ads_blocked"] = number_format(
+                    $details->ads_blocked_today
+                );
+                $data["ads_percentage"] = number_format(
+                    $details->ads_percentage_today,
+                    1
+                );
+                $status = "active";
+            }
         }
 
+        if ($version == 6) {
+            $auth = $this->login();
+
+            $attrs = [
+                "body" => "sid:" . $auth->sid,
+                "headers" => [
+                    "content-type" => "application/json",
+                    "accept" => "application/json",
+                ],
+            ];
+            $result = parent::execute($this->url("api/stats/summary"),$attrs);
+
+            $data["ads_blocked"] = $result->blocked;
+            $data["ads_percentage"] = $result->percent_blocked;
+        }
         return parent::getLiveStats($status, $data);
     }
     public function url($endpoint)
     {
-        $apiKey = $this->config->apikey;
-        $api_url = parent::normaliseurl($this->config->url) . $endpoint;
+        $version = $this->config->version;
+        if ($version == 5){
+            $apiKey = $this->config->apikey;
+            $api_url = parent::normaliseurl($this->config->url) . $endpoint;
 
-        if ($apiKey) {
-            $api_url .= "&auth=" . $apiKey;
+            if ($apiKey) {
+                $api_url .= "&auth=" . $apiKey;
+            }
         }
-
+        if ($version == 6) {
+            $api_url = parent::normaliseurl($this->config->url) . $endpoint;
+        }
         return $api_url;
+    }
+    public function login()
+    {
+        $attrs = [
+            "body" => "password:" . $this->config->apiKey,
+            "headers" => [
+                "content-type" => "application/json",
+                "accept" => "application/json",
+            ],
+        ];
+        return parent::execute(
+            $this->url("api/auth"),
+            $attrs,
+            null,
+            "POST"
+        );
     }
 }
--- Pihole/config.blade.php
+++ Pihole/config.blade.php
@@ -6,9 +6,20 @@
 		{!! Form::text('config[override_url]', isset($item) ? $item->getconfig()->override_url : null, ['placeholder' => __('app.apps.override'), 'id' => 'override_url', 'class' => 'form-control']) !!}
 	</div>
 	<div class="input">
-		<label>{{ __('app.apps.apikey') }}</label>
+        <label>Api Key/App Password(v6)</label>
 		{!! Form::text('config[apikey]', isset($item) && property_exists($item->getconfig(), 'apikey') ? $item->getconfig()->apikey : null, ['placeholder' => __('app.apps.apikey'), 'data-config' => 'apikey', 'class' => 'form-control config-item']) !!}
 	</div>
+<div class="items">
+    <div class="input">
+        <label>Version</label>
+        {!! Form::select(
+            'config[version]',
+            ['5' => 'v5', '6' => 'v6'],
+            isset($item) ? $item->getconfig()->version : null,
+            ['data-config' => 'version', 'class' => 'form-control config-item'],
+        ) !!}
+    </div>
+</div>
 	<div class="input">
 		<button style="margin-top: 32px;" class="btn test" id="test_config">Test</button>
 	</div>
