089d013574e580a1491858a06152d1224832df19
Pedro Buffon <henriquebuffon@gmail.com>
2024-09-28 03:15:50 +0000

    fix whitespace

--- Pihole/Pihole.php
+++ Pihole/Pihole.php
@@ -28,7 +28,8 @@ class Pihole extends \App\SupportedApps implements \App\EnhancedApps
             $test = $this->getInfo();
             if ($test["valid"]) {
                 echo "Successfully communicated with the API";
-            }else {
+            }
+            else {
                 echo "Error while communicating with the API";
             }
         }
