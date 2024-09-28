66a48667cd323b1121b9d8027081e998fb069df1
Pedro Buffon <henriquebuffon@gmail.com>
2024-09-28 08:21:03 -0300

    Enhanced pi hole (#779)
    * refactored to work with v5 and v6
    
    * working code for v5 and v6
    
    * fix whitespaces
    
    * fix whitespace
    
    * whitespace
    
    * Update config.blade.php

--- Pihole/Pihole.php
+++ Pihole/Pihole.php
@@ -13,44 +13,128 @@ class Pihole extends \App\SupportedApps implements \App\EnhancedApps
 
     public function __construct()
     {
-        //$this->jar = new \GuzzleHttp\Cookie\CookieJar; // Uncomment if cookies need to be set
+        $this->jar = new \GuzzleHttp\Cookie\CookieJar(); // Uncomment if cookies need to be set
     }
 
     public function test()
     {
-        $test = parent::appTest($this->url("api.php?summaryRaw"));
-        echo $test->status;
-    }
+        $version = $this->config->version;
 
+        if ($version == 5) {
+            $test = parent::appTest($this->url("api.php?summaryRaw"));
+            echo $test->status;
+        }
+        if ($version == 6) {
+            $test = $this->getInfo();
+            if ($test["valid"]) {
+                echo "Successfully communicated with the API";
+            } else {
+                echo "Error while communicating with the API";
+            }
+        }
+    }
     public function livestats()
     {
-        $status = "inactive";
-        $res = parent::execute($this->url("api.php?summaryRaw"));
-        $details = json_decode($res->getBody());
+        $version = $this->config->version;
 
-        $data = [];
+        if ($version == 5) {
+            $res = parent::execute($this->url("api.php?summaryRaw"));
+            $details = json_decode($res->getBody());
+            if ($details) {
+                $data["ads_blocked"] = number_format(
+                    $details->ads_blocked_today
+                );
+                $data["ads_percentage"] = number_format(
+                    $details->ads_percentage_today,
+                    1
+                );
+                $data["gravity"] = number_format(
+                    $details->domains_being_blocked,
+                    0,
+                    '',
+                    '.'
+                );
 
-        if ($details) {
-            $data["ads_blocked_today"] = number_format(
-                $details->ads_blocked_today
-            );
-            $data["ads_percentage_today"] = number_format(
-                $details->ads_percentage_today,
-                1
-            );
+                $status = "active";
+            }
         }
 
+        if ($version == 6) {
+            $results = $this->getInfo();
+
+            $data["ads_blocked"] = $results["queries"];
+            $data["ads_percentage"] = $results["percent"];
+            $data["gravity"] = $results["gravity"];
+
+            $status = "active";
+        }
         return parent::getLiveStats($status, $data);
     }
+
     public function url($endpoint)
     {
-        $apiKey = $this->config->apikey;
-        $api_url = parent::normaliseurl($this->config->url) . $endpoint;
+        $version = $this->config->version;
+        if ($version == 5) {
+            $apikey = $this->config->apikey;
+            $api_url = parent::normaliseurl($this->config->url) . $endpoint;
 
-        if ($apiKey) {
-            $api_url .= "&auth=" . $apiKey;
+            if ($apikey) {
+                $api_url .= "&auth=" . $apikey;
+            }
         }
-
+        if ($version == 6) {
+            $api_url = parent::normaliseurl($this->config->url) . $endpoint;
+        }
         return $api_url;
+    }
+
+    public function getInfo()
+    {
+        $attrs = [
+            "body" => json_encode(['password' => $this->config->apikey]),
+            "cookies" => $this->jar,
+            "headers" => [
+                "Content-Type" => "application/json",
+                "Accept" => "application/json",
+            ],
+        ];
+
+        // Create session and retreave data
+        $response = parent::execute($this->url("api/auth"), $attrs, null, "POST");
+        $auth = json_decode($response->getBody());
+
+        $attrsid = [
+            "body" => json_encode(['sid' => $auth->session->sid]),
+            "cookies" => $this->jar,
+            "headers" => [
+                "Content-Type" => "application/json",
+                "Accept" => "application/json",
+            ],
+        ];
+
+        // Get queries data
+        $responsesummary = parent::execute($this->url("api/stats/summary"), $attrsid, null, "GET");
+        $datasummary = json_decode($responsesummary->getBody());
+
+        // After retrieving the data the session is closed to declutter
+        parent::execute($this->url("api/auth"), $attrsid, null, "DELETE");
+
+        // Extract data from the response
+        $valid = $auth->session->valid;
+        $validity = $auth->session->validity;
+        $message = $auth->session->message;
+        $queriesblocked = $datasummary->queries->blocked;
+        $percentblocked = round($datasummary->queries->percent_blocked, 2);
+        $gravity = number_format($datasummary->gravity->domains_being_blocked, 0, '', '.');
+
+        $data = [
+            'valid'    => $valid,
+            'validity' => $validity,
+            'message'  => $message,
+            'queries'  => $queriesblocked,
+            'percent'  => $percentblocked,
+            'gravity'  => $gravity
+        ];
+        return $data;
     }
 }
--- Pihole/livestats.blade.php
+++ Pihole/livestats.blade.php
@@ -1,10 +1,14 @@
 <ul class="livestats">
     <li>
         <span class="title">Queries<br />Blocked</span>
-        <strong>{!! $ads_blocked_today !!}</strong>
+        <strong>{!! $ads_blocked !!}</strong>
     </li>
     <li>
         <span class="title">Percent<br /> Blocked</span>
-        <strong>{!! $ads_percentage_today !!}</strong>
+        <strong>{!! $ads_percentage !!}</strong>
+    </li>
+    <li>
+        <span class="title">Domains<br /> Blocked</span>
+        <strong>{!! $gravity !!}</strong>
     </li>
 </ul>
