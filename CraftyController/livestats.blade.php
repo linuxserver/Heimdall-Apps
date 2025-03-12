335ee90a3f6edbaa449cf48c68b8201c30f1f8f6
Sander <scebbers91@gmail.com>
2025-03-13 00:01:56 +0100

    Add files via upload

---
+++ CraftyController/livestats.blade.php
@@ -0,0 +1,10 @@
+<ul class="livestats">
+    <li>
+        <span class="title">servers</span>
+        <strong>{!! $servers_total !!}</strong>
+    </li>
+    <li>
+        <span class="title">online</span>
+        <strong>{!! $servers_online !!}</strong>
+    </li>
+</ul>
