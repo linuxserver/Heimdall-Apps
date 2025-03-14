c827d3688a6059818d3b91e2b248f763c49b59af
root <root@TrueNAS.local>
2025-03-14 11:35:38 +0100

    Updated config and livestats

--- CraftyController/livestats.blade.php
+++ CraftyController/livestats.blade.php
@@ -1,10 +1,10 @@
 <ul class="livestats">
     <li>
-        <span class="title">servers: {!! $servers_total !!}</span>
-        <span class="title">players: {!! $players_online !!}</span>
+        <span class="title">servers:</span><strong>{!! $servers_total !!}</strong>
+        <span class="title">players:</span><strong>{!! $players_online !!}</strong>
     </li>
     <li>
-        <span class="title">online: {!! $servers_online !!}</span>
-        <span class="title">{!! $mem !!} GB</span>
+        <span class="title">online:</span><strong>{!! $servers_online !!}</strong>
+        <span class="title">memory:</span><strong>{!! $mem !!} GB</strong>
     </li>
 </ul>
