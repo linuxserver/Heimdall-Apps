a2daca1a430c3dc3efdbb90553e0a3e91ac8e5b7
GOUKI9999 <gouki9999@hotmail.com>
2025-08-17 13:21:42 +0800

    PHP-CS fixing

--- WhatsupDocker/WhatsupDocker.php
+++ WhatsupDocker/WhatsupDocker.php
@@ -4,7 +4,6 @@ namespace App\SupportedApps\WhatsupDocker;
 
 class WhatsupDocker extends \App\SupportedApps implements \App\EnhancedApps
 {
-
     public $config;
 
     //protected $login_first = true; // Uncomment if api requests need to be authed first
@@ -32,41 +31,41 @@ class WhatsupDocker extends \App\SupportedApps implements \App\EnhancedApps
     public function livestats()
     {
         $status = 'inactive';
-        
+
         try {
             // Get watched containers from WUD API
             $res = parent::execute($this->url('api/containers'), $this->getAttrs());
             $containers = json_decode($res->getBody(), true);
-            
+
             if ($containers && is_array($containers)) {
                 $status = 'active';
-                
+
                 // Calculate container statistics
                 $totalContainers = count($containers);
                 $updatableContainers = 0;
                 $watchedContainers = 0;
-                
+
                 foreach ($containers as $container) {
                     // Count updatable containers (based on API doc structure)
                     if (isset($container['updateAvailable']) && $container['updateAvailable'] === true) {
                         $updatableContainers++;
                     }
-                    
+
                     // All containers returned by /api/containers are watched containers
                     $watchedContainers++;
                 }
-                
+
                 $details = ['visiblestats' => []];
-                
+
                 // Show configured stats, or all if none selected
-                $availableStats = isset($this->config->availablestats) && !empty($this->config->availablestats) 
-                    ? $this->config->availablestats 
+                $availableStats = isset($this->config->availablestats) && !empty($this->config->availablestats)
+                    ? $this->config->availablestats
                     : array_keys(self::getAvailableStats());
-                    
+
                 foreach ($availableStats as $stat) {
                     $newstat = new \stdClass();
                     $newstat->title = self::getAvailableStats()[$stat];
-                    
+
                     switch ($stat) {
                         case 'TotalContainers':
                             $newstat->value = $totalContainers;
@@ -80,10 +79,10 @@ class WhatsupDocker extends \App\SupportedApps implements \App\EnhancedApps
                         default:
                             $newstat->value = 0;
                     }
-                    
+
                     $details['visiblestats'][] = $newstat;
                 }
-                
+
                 return parent::getLiveStats($status, $details);
             }
         } catch (Exception $e) {
@@ -102,7 +101,7 @@ class WhatsupDocker extends \App\SupportedApps implements \App\EnhancedApps
                 }
             }
         }
-        
+
         return parent::getLiveStats($status, []);
     }
 
@@ -119,12 +118,12 @@ class WhatsupDocker extends \App\SupportedApps implements \App\EnhancedApps
                 'Content-Type' => 'application/json',
             ]
         ];
-        
+
         // Add basic authentication if both username and password are configured
         if ($this->hasAuthCredentials()) {
             $attrs['auth'] = [$this->config->username, $this->config->password];
         }
-        
+
         return $attrs;
     }
 
