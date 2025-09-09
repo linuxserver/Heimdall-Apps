211f9349c14f1c9b43ecabaa8df7405e98f6b717
Lucas Araujo <lucas.ribeiro.de.araujo@rabobank.nl>
2025-09-09 12:10:27 +0200

    feat(Audiobookshelf): enhance stats display and add book count
    - Rename "Total Playtime" to "Playtime" for brevity
    - Add book count to stats display
    - Simplify time format by removing seconds
    - Clean up constructor and unused code
    - Update API endpoint for testing

--- Audiobookshelf/Audiobookshelf.php
+++ Audiobookshelf/Audiobookshelf.php
@@ -6,17 +6,11 @@ class Audiobookshelf extends \App\SupportedApps implements \App\EnhancedApps
 {
     public $config;
 
-    //protected $login_first = true; // Uncomment if api requests need to be authed first
-    //protected $method = 'POST';  // Uncomment if requests to the API should be set by POST
-
-    public function __construct()
-    {
-        //$this->jar = new \GuzzleHttp\Cookie\CookieJar; // Uncomment if cookies need to be set
-    }
+    public function __construct() {}
 
     public function test()
     {
-        $test = parent::appTest($this->url('status'));
+        $test = parent::appTest($this->url('api/me'), $this->getAttrs());
         echo $test->status;
     }
 
@@ -25,7 +19,15 @@ class Audiobookshelf extends \App\SupportedApps implements \App\EnhancedApps
         $status = 'inactive';
         $res = parent::execute($this->url('api/me/listening-stats'), $this->getAttrs());
         $details = json_decode($res->getBody());
-        $data = ['totalTime' => $this->secondsToHoursMinutes($details->totalTime)];
+
+        if ($details->totalTime > 0) {
+            $status = 'active';
+            $data = [
+                'totalTime' => $this->secondsToHoursMinutes($details->totalTime),
+                'bookCount' => $this->get_book_count()
+            ];
+        }
+
         return parent::getLiveStats($status, $data);
     }
 
@@ -33,6 +35,22 @@ class Audiobookshelf extends \App\SupportedApps implements \App\EnhancedApps
     {
         $api_url = parent::normaliseurl($this->config->url) . $endpoint;
         return $api_url;
+    }
+
+    private function get_book_count()
+    {
+        $res = parent::execute($this->url('api/libraries'), $this->getAttrs());
+        $libraries = json_decode($res->getBody())->libraries;
+
+        $book_count = 0;
+        foreach ($libraries as $library) {
+            $lib_id = $library->id;
+            $res = parent::execute($this->url('api/libraries/' . $lib_id . '/items'), $this->getAttrs());
+            $library_details = json_decode($res->getBody());
+            $book_count += $library_details->total;
+        }
+
+        return $book_count;
     }
 
     private function getAttrs()
@@ -46,12 +64,10 @@ class Audiobookshelf extends \App\SupportedApps implements \App\EnhancedApps
         ];
     }
 
-
     private function secondsToHoursMinutes($seconds)
     {
         $hours = floor($seconds / 3600);
         $minutes = floor(($seconds % 3600) / 60);
-        $return_seconds = floor($seconds % 60);
-        return $hours . 'h ' . $minutes . 'min ' . $return_seconds . 'sec';
+        return $hours . 'h ' . $minutes . 'm';
     }
 }
--- Audiobookshelf/livestats.blade.php
+++ Audiobookshelf/livestats.blade.php
@@ -1,6 +1,10 @@
 <ul class="livestats">
     <li>
-        <span class="title">Total Playtime</span>
+        <span class="title">Playtime</span>
         <strong>{!! $totalTime !!}</strong>
+    </li>
+    <li>
+        <span class="title"># Books</span>
+        <strong>{!! $bookCount !!}</strong>
     </li>
 </ul>
