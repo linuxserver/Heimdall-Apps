3eed80d66c55048450d12ee9d4858cce71114d29
Pedro Buffon <henriquebuffon@gmail.com>
2025-11-11 13:41:37 +0000

    N8n enhanced version

---
+++ N8n/livestats.blade.php
@@ -0,0 +1,10 @@
+<ul class="livestats">
+    <li>
+        <span class="title">Workflows</span>
+        <strong>{!! $workflows !!}</strong>
+    </li>
+    <li>
+        <span class="title">Active</span>
+        <strong>{!! $active !!}</strong>
+    </li>
+</ul>
