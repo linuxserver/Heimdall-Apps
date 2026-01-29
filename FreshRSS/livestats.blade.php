878a9af2c4f4bd02f95ae8789d818d6d83866f51
Jay Collett <486430+jaycollett@users.noreply.github.com>
2026-01-29 12:40:40 -0500

    Fix FreshRSS livestats styling to match other apps
    Update the livestats.blade.php template to use the standard pattern
    with separate title and strong elements, consistent with Miniflux,
    Sonarr, Plex, and other apps.
    
    Fixes linuxserver/Heimdall#1525

--- FreshRSS/livestats.blade.php
+++ FreshRSS/livestats.blade.php
@@ -1,7 +1,8 @@
 <ul class="livestats">
     @if ($unread > 0)
         <li>
-            <span class="title" style="color:orange;">{!! $unread !!} Unread</span>
+            <span class="title">Unread</span>
+            <strong>{!! $unread !!}</strong>
         </li>
     @endif
 </ul>
