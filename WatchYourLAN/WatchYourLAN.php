0b401bb3399d66277d33b6a184bb82bea950d118
Keith <git.stuff@kthomas.me.uk>
2026-02-20 17:44:02 +0000

    WatchYourLAN enhanced version

--- WatchYourLAN/WatchYourLAN.php
+++ WatchYourLAN/WatchYourLAN.php
@@ -2,6 +2,47 @@
 
 namespace App\SupportedApps\WatchYourLAN;
 
-class WatchYourLAN extends \App\SupportedApps
+class WatchYourLAN extends \App\SupportedApps implements \App\EnhancedApps
 {
+
+    public $config;
+
+    //protected $login_first = true; // Uncomment if api requests need to be authed first
+    //protected $method = 'POST';  // Uncomment if requests to the API should be set by POST
+
+    public function __construct()
+    {
+        //$this->jar = new \GuzzleHttp\Cookie\CookieJar; // Uncomment if cookies need to be set
+    }
+
+    public function test()
+    {
+        $test = parent::appTest($this->url('api/all/'));
+        echo $test->status;
+    }
+
+    public function livestats()
+    {
+        $status = 'inactive';
+        $res = parent::execute($this->url('api/all/'));
+        $hosts = json_decode($res->getBody());
+        $unknown_count = 0;
+
+        if (is_array($hosts)) {
+          foreach ($hosts as $key => $host) {
+            if (isset($host->Known) && $host->Known == 0) {
+                $unknown_count += 1;
+            }
+          }
+        }
+
+        $data['unknown_count'] = $unknown_count;
+        return parent::getLiveStats($status, $data);
+    }
+
+    public function url($endpoint)
+    {
+        $api_url = parent::normaliseurl($this->config->url) . $endpoint;
+        return $api_url;
+    }
 }
