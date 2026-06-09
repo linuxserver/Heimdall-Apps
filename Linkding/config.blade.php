c63c004d0ea2e4c43ac7488647856b52182b8837
keithy-t <github@kthomas.me.uk>
2026-06-09 15:19:52 +0100

    Update config.blade.php

--- Linkding/config.blade.php
+++ Linkding/config.blade.php
@@ -2,14 +2,13 @@
 <div class="items">
     <div class="input">
         <label>{{ strtoupper(__('app.url')) }}</label>
-        {!! Form::text('config[override_url]', null, array('placeholder' => __('app.apps.override'), 'id' => 'override_url', 'class' => 'form-control')) !!}
+        {!! Form::text('config[override_url]', isset($item) ? $item->getconfig()->override_url : null, array('placeholder' => __('app.apps.override'), 'id' => 'override_url', 'class' => 'form-control')) !!}
     </div>
     <div class="input">
-        <label>Access Token</label>
-        {!! Form::text('config[access_token]', null, array('placeholder' => 'Access Token', 'data-config' => 'access_token', 'class' => 'form-control config-item')) !!}
+        <label>{{ __('app.apps.apikey') }}</label>
+        {!! Form::text('config[apikey]', isset($item) ? $item->getconfig()->apikey : null, array('placeholder' => __('app.apps.apikey'), 'data-config' => 'apikey', 'class' => 'form-control config-item')) !!}
     </div>
     <div class="input">
         <button style="margin-top: 32px;" class="btn test" id="test_config">Test</button>
     </div>
 </div>
-
