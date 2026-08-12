8118e86a495a29a32f1fc7e313d83495b3ceee10
Radu Galbenu <radugalbenu@gmail.com>
2026-08-12 20:05:16 +0300

    Tracearr: add enhanced support with stream/transcode/bandwidth stats
    Auths with a Tracearr public API key (Bearer), hits
    /api/v2/public/streams?summary=true, and surfaces active streams,
    active transcodes, and total bandwidth on the dashboard tile.

--- Tracearr/Tracearr.php
+++ Tracearr/Tracearr.php
@@ -2,6 +2,52 @@
 
 namespace App\SupportedApps\Tracearr;
 
-class Tracearr extends \App\SupportedApps
+class Tracearr extends \App\SupportedApps implements \App\EnhancedApps
 {
+    public $config;
+
+    public function test()
+    {
+        $test = parent::appTest($this->url("api/v2/public/streams?summary=true"), $this->getAttrs());
+        echo $test->status;
+    }
+
+    public function livestats()
+    {
+        $status = "inactive";
+        $data = [];
+
+        $res = parent::execute($this->url("api/v2/public/streams?summary=true"), $this->getAttrs());
+        $details = json_decode($res->getBody());
+        $summary = $details->summary ?? null;
+
+        if ($summary) {
+            $data = [
+                "streams" => $summary->total,
+                "transcodes" => $summary->transcodes,
+                "bandwidth" => $summary->total_bitrate,
+            ];
+            if ($summary->total > 0) {
+                $status = "active";
+            }
+        }
+
+        return parent::getLiveStats($status, $data);
+    }
+
+    public function url($endpoint)
+    {
+        $api_url = parent::normaliseurl($this->config->url) . $endpoint;
+        return $api_url;
+    }
+
+    private function getAttrs()
+    {
+        return [
+            "headers" => [
+                "Accept" => "application/json",
+                "Authorization" => "Bearer " . $this->config->apikey,
+            ],
+        ];
+    }
 }
---
+++ Tracearr/livestats.blade.php
@@ -0,0 +1,14 @@
+<ul class="livestats">
+    <li>
+        <span class="title">Streams</span>
+        <strong>{!! $streams !!}</strong>
+    </li>
+    <li>
+        <span class="title">Transcodes</span>
+        <strong>{!! $transcodes !!}</strong>
+    </li>
+    <li>
+        <span class="title">Bandwidth</span>
+        <strong>{!! $bandwidth !!}</strong>
+    </li>
+</ul>
