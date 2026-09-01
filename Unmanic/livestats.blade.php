3fabe9296969a7f03c83ee1fd6bd383e789c9ef6
StonyTark1117 <braydonsanders2001@gmail.com>
2026-08-31 19:13:51 -0700

    Add enhanced Unmanic live stats

---
+++ Unmanic/livestats.blade.php
@@ -0,0 +1,14 @@
+<ul class="livestats">
+    <li>
+        <span class="title">Pending</span>
+        <strong>{!! $pending !!}</strong>
+    </li>
+    <li>
+        <span class="title">Workers</span>
+        <strong>{!! $workers !!}</strong>
+    </li>
+    <li>
+        <span class="title">Completed</span>
+        <strong>{!! $completed !!}</strong>
+    </li>
+</ul>
